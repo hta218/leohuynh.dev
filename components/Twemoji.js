2866302dee85253a537f1165abd101d06de66113
Leo Huynh <huynhtuananh21895@gmail.com>
2021-07-25 09:32:03 +0700

    Add twemoji

---
+++ components/Twemoji.js
@@ -0,0 +1,8 @@
+const { default: kebabCase } = require('@/lib/utils/kebabCase')
+
+const Twemoji = ({ emoji }) => {
+  const twaClass = kebabCase(emoji)
+  return <i className={`twa twa-lg twa-${twaClass}`} />
+}
+
+export default Twemoji
