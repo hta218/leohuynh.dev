401f22a118a8737b1f50fbb28bb2191cdba9b103
Leo Huynh <huynhtuananh21895@gmail.com>
2021-07-25 16:59:37 +0700

    Add images, update post

--- components/Image.js
+++ components/Image.js
@@ -1,6 +1,10 @@
 import NextImage from 'next/image'
 
 // eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ ...rest }) => <NextImage {...rest} />
+const Image = ({ ...rest }) => (
+  <div className="flex justify-center">
+    <NextImage {...rest} />
+  </div>
+)
 
 export default Image
