09d3aafe15719b3929da07c1a30e79f6dc6657d0
Leo Huynh <huynhtuananh21895@gmail.com>
2021-07-29 00:47:50 +0700

    New post job part 2

--- components/Twemoji.js
+++ components/Twemoji.js
@@ -1,8 +1,8 @@
 import kebabCase from '@/lib/utils/kebabCase'
 
-const Twemoji = ({ emoji }) => {
+const Twemoji = ({ emoji, size = 'twa-lg' }) => {
   const twaClass = kebabCase(emoji)
-  return <i className={`twa twa-lg twa-${twaClass}`} />
+  return <i className={`twa ${size} twa-${twaClass}`} />
 }
 
 export default Twemoji
