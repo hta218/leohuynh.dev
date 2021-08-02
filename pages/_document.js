f4b8b77c2c9f4fdbcd7e7018e5a21628ad5b0fae
Leo Huynh <huynhtuananh21895@gmail.com>
2021-08-02 09:04:18 +0700

    Update code style

--- pages/_document.js
+++ pages/_document.js
@@ -23,9 +23,9 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap"
             rel="stylesheet"
           />
           {/* <link
