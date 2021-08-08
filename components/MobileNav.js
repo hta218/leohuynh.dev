ba56dd86f4e4059f0214492de0366c4158465a40
Leo Huynh <huynhtuananh21895@gmail.com>
2021-08-09 00:46:14 +0700

    Update mobile nav

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -47,7 +47,7 @@ const MobileNav = () => {
         </svg>
       </button>
       <div
-        className={`fixed w-full h-full top-24 right-0 bg-gray-200 dark:bg-gray-800 opacity-95 z-10 transform ease-in-out duration-300 ${
+        className={`fixed w-full h-full top-24 right-0 bg-gray-200 dark:bg-gray-800 opacity-95 z-10 transition-transform transform ease-in-out duration-300 ${
           navShow ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
