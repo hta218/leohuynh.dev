88b75972fb5ae2e2f21730883be19e6cf4fcd3d9
Leo Huynh <huynhtuananh21895@gmail.com>
2021-08-08 14:49:14 +0700

    Update layout

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -17,7 +16,6 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label="Leo's Blog">
               <div className="flex items-center justify-between">
                 <div className="mr-3 flex justify-center items-center">
-                  {/* <Logo /> */}
                   <NextImage
                     src="/static/images/logo.jpg"
                     width={50}
