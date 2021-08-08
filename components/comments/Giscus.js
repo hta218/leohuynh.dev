b71c7c47cc955675a94488e9cfdb1019184d6599
Leo Huynh <huynhtuananh21895@gmail.com>
2021-08-08 15:02:12 +0700

    Update giscus

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -45,7 +45,6 @@ const Giscus = ({ mapping }) => {
 
   return (
     <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300">
-      {/* {enableLoadComments && <button onClick={LoadComments}>Load Comments</button>} */}
       <div className="giscus" id={COMMENTS_ID} />
     </div>
   )
