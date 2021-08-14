1c78d2638b68d000c0d0c7eee856ad0a79ebb77a
Leo Huynh <huynhtuananh21895@gmail.com>
2021-08-15 00:01:36 +0700

    Update projects

--- pages/projects.js
+++ pages/projects.js
@@ -15,7 +15,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Things that I made in my free time
+            My open source tools and side projects
           </p>
         </div>
         <div className="container py-12">
