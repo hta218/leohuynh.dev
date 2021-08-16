faa7bb706bd3eead398a0f927c1342af152a73a4
Leo Huynh <huynhtuananh21895@gmail.com>
2021-08-17 00:23:32 +0700

    Update project data and project page

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,10 +1,25 @@
 const projectsData = [
   {
+    title: 'Minimog - The Next Generation Shopify Theme',
+    description: `The Next Generation of highest-converting and extensible Shopify's theme that contains 50+ demos, mobile-first, and features rich! (Top Trending in eCommerce category and #2 Weekly Bestseller on the first release week)`,
+    imgSrc: '/static/images/minimog.jpg',
+    href: 'https://themeforest.net/item/minimog-the-high-converting-shopify-theme/33380968',
+    type: 'work',
+  },
+  {
+    title: 'Fox Kit Shopify App',
+    description: `The upsells and boost conversion tools that is built to support Minimog theme (Made with Koa.js, JWT, MongoDB, Shopify-koa-auth and Polaris + React)`,
+    imgSrc: '/static/images/foxkit.png',
+    href: 'https://apps.shopify.com/foxecom-boost-sales',
+    type: 'work',
+  },
+  {
     title: 'Shopify theme starter',
-    description: `Starter kit to build a Shopify Theme from scratch includes Webpack 5 and Tailwind CSS 2`,
+    description: `Starter kit to build a Shopify Theme from scratch includes Webpack 5 and Tailwind CSS 2 (Minimog theme use this)`,
     imgSrc: '/static/images/shopify.jpg',
     href: 'https://github.com/hta218/shopify-theme-starter',
     repoName: 'shopify-theme-starter',
+    type: 'self',
   },
   {
     title: 'Tiny retry',
@@ -12,6 +27,7 @@ const projectsData = [
     imgSrc: '/static/images/tiny-retry.png',
     href: 'https://github.com/hta218/tiny-retry',
     repoName: 'tiny-retry',
+    type: 'self',
   },
   {
     title: 'Travel Egypt Picture Puzzle',
@@ -19,6 +35,7 @@ const projectsData = [
     imgSrc: '/static/images/travel-egypt.png',
     href: 'https://github.com/hta218/Travel_Egypt',
     repoName: 'Travel_Egypt',
+    type: 'self',
   },
 ]
 
