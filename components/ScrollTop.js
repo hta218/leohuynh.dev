909d59163111e7446974112764db183bd68e8d67
Leo Huynh <huynhtuananh21895@gmail.com>
2021-08-17 21:40:32 +0700

    Update ScrollTop button

--- components/ScrollTop.js
+++ components/ScrollTop.js
@@ -1,9 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import smoothscroll from 'smoothscroll-polyfill'
 
 const ScrollTop = () => {
+  const [show, setShow] = useState(false)
+
   useEffect(() => {
     smoothscroll.polyfill()
+    const handleWindowScroll = () => {
+      if (window.scrollY > 200) setShow(true)
+      else setShow(false)
+    }
+
+    window.addEventListener('scroll', handleWindowScroll)
+    return () => window.removeEventListener('scroll', handleWindowScroll)
   }, [])
 
   const handleClick = () => {
@@ -15,10 +24,11 @@ const ScrollTop = () => {
       aria-label="Scroll To Top"
       type="button"
       onClick={handleClick}
-      className="hidden md:inline-block fixed right-8 bottom-8 rounded-md bg-gray-800 text-gray-100 p-3"
+      style={{ opacity: show ? 1 : 0 }}
+      className="hidden md:inline-block transition-opacity fixed right-8 bottom-8 rounded-md bg-gray-800 hover:bg-gray-700 text-gray-100 p-2"
     >
       <svg
-        className="w-6 h-6"
+        className="w-[22px] h-[22px]"
         fill="none"
         stroke="currentColor"
         viewBox="0 0 24 24"
