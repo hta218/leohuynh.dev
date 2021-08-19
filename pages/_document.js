8ed98ce5495a3b777d7d79d5e9113f79d68e5214
Leo Huynh <huynhtuananh21895@gmail.com>
2021-08-20 01:10:18 +0700

    Update next document

--- pages/_document.js
+++ pages/_document.js
@@ -4,16 +4,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/eagle.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/eagle.png" />
-          {/* <link
-            rel="icon"
-            type="image/jpg"
-            sizes="16x16"
-            href="/static/favicons/favicon-16x16.jpg"
-          /> */}
+          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/hat.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/hat.png" />
+          <link rel="icon" type="image/jpg" sizes="16x16" href="/static/favicons/hat.jpg" />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/static/favicons/eagle.png" color="#fff" />
+          <link rel="mask-icon" href="/static/favicons/hat.png" color="#fff" />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
