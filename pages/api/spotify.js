ba08eb3ad35c75b05b798b74a52bb2d0eb5e6828
Leo Huynh <huynhtuananh21895@gmail.com>
2021-09-05 21:27:57 +0700

    Update spotify api

--- pages/api/spotify.js
+++ pages/api/spotify.js
@@ -1,6 +1,6 @@
 import { getNowPlaying } from '@/lib/spotify'
 
-export default async (_, res) => {
+export default async (req, res) => {
   const response = await getNowPlaying()
 
   if (response.status === 204 || response.status > 400) {
@@ -10,7 +10,7 @@ export default async (_, res) => {
   const song = await response.json()
   const isPlaying = song.is_playing
   const title = song.item.name
-  const artist = song.item.artists.map((_artist) => _artist.name).join(', ')
+  const artist = song.item.artists.map((art) => art.name).join(', ')
   const album = song.item.album.name
   const albumImageUrl = song.item.album.images[0].url
   const songUrl = song.item.external_urls.spotify
