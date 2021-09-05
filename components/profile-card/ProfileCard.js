4d712403f40293b6f4165999f6b3b0ab74a81f63
Leo Huynh <huynhtuananh21895@gmail.com>
2021-09-05 17:41:02 +0700

    Update spotify to card

--- components/profile-card/ProfileCard.js
+++ components/profile-card/ProfileCard.js
@@ -2,6 +2,7 @@ import Image from '@/components/Image'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import useSWR from 'swr'
 import fetcher from '@/lib/fetcher'
+import SpotifyIcon from '@/components/dev-icons/spotify.svg'
 
 const ProfileCard = ({ avatar }) => {
   const { data } = useSWR('/api/spotify', fetcher)
@@ -68,19 +69,36 @@ const ProfileCard = ({ avatar }) => {
 
           <div className="flex items-center px-3 xl:px-6 py-2 bg-gray-900">
             <svg
-              className="w-6 h-6 text-white fill-current"
+              fill="#1DB954"
+              className="w-6 h-6"
+              role="img"
               viewBox="0 0 24 24"
-              fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M17 21C15.8954 21 15 20.1046 15 19V15C15 13.8954 15.8954 13 17 13H19V12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12V13H7C8.10457 13 9 13.8954 9 15V19C9 20.1046 8.10457 21 7 21H3V12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12V21H17ZM19 15H17V19H19V15ZM7 15H5V19H7V15Z"
-              />
+              <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z" />
             </svg>
 
-            <h1 className="mx-3 text-lg font-semibold text-white">Focusing</h1>
+            <div className="inline-flex ml-2 flex-col sm:flex-row w-full max-w-full truncate">
+              {data?.songUrl ? (
+                <>
+                  {/* <span className="text-gray-500 dark:text-gray-300">Listening to:</span> */}
+                  <a
+                    className="ml-1 text-gray-800 dark:text-gray-200 font-medium"
+                    href={data.songUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {data.title}
+                  </a>
+                </>
+              ) : (
+                <p className="text-gray-800 dark:text-gray-200 font-medium">Not Playing</p>
+              )}
+              <span className="mx-2 text-gray-500 dark:text-gray-300 hidden sm:block">{' – '}</span>
+              <p className="text-gray-500 dark:text-gray-300 max-w-max truncate">
+                {data?.artist ?? 'Spotify'}
+              </p>
+            </div>
           </div>
 
           <div className="hidden xl:block xl:px-6 py-4">
