df0112d38029918c40907d1588aa635234d3d349
Leo Huynh <huynhtuananh21895@gmail.com>
2021-09-05 17:21:37 +0700

    Add api

---
+++ pages/api/spotify.js
@@ -0,0 +1,26 @@
+import { getNowPlaying } from '@/lib/spotify'
+
+export default async (_, res) => {
+  const response = await getNowPlaying()
+
+  if (response.status === 204 || response.status > 400) {
+    return res.status(200).json({ isPlaying: false })
+  }
+
+  const song = await response.json()
+  const isPlaying = song.is_playing
+  const title = song.item.name
+  const artist = song.item.artists.map((_artist) => _artist.name).join(', ')
+  const album = song.item.album.name
+  const albumImageUrl = song.item.album.images[0].url
+  const songUrl = song.item.external_urls.spotify
+
+  return res.status(200).json({
+    album,
+    albumImageUrl,
+    artist,
+    isPlaying,
+    songUrl,
+    title,
+  })
+}
