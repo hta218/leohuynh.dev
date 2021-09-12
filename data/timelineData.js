40f5dbc70926cc48c068a54e6f40accf591f5539
Leo Huynh <huynhtuananh21895@gmail.com>
2021-09-12 16:48:44 +0700

    Add timeline data

---
+++ data/timelineData.js
@@ -0,0 +1,124 @@
+const timelineData = [
+  {
+    time: 'May 2021 - now',
+    role: 'Software Engineer',
+    company: 'Insights Studio',
+    companyURL: 'https://insights.is/',
+    works: [
+      <>
+        Built{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://themeforest.net/item/minimog-the-high-converting-shopify-theme/33380968"
+        >
+          Minimog
+        </a>{' '}
+        - The Next Generation Shopify Theme (Weekly Bestseller and Top Trending in eCommerce
+        category)
+      </>,
+      <>
+        Built{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://apps.shopify.com/foxecom-boost-sales"
+        >
+          Fox Kit Shopify App
+        </a>{' '}
+        - The upsells and boost conversion tools that is built to support Minimog theme
+      </>,
+    ],
+  },
+  {
+    time: 'May 2020 - Apr 2020',
+    role: 'Frontend Engineer',
+    company: 'Coc Coc',
+    companyURL: 'https://coccoc.com/',
+    companyBio: '#1 web browser in Viet Nam with more than 25M users',
+    works: [
+      <>
+        Built{' '}
+        <a target="_blank" rel="noopener noreferrer" href="https://coccoc.com/webhp">
+          Coc Coc's newsfeed
+        </a>{' '}
+        (with Team Leader)
+      </>,
+      `Maintain and optimize Newtab's widget and performance`,
+    ],
+  },
+  {
+    time: 'Aug 2018 - Apr 2020',
+    role: 'Fullstack Developer',
+    company: 'Pagefly',
+    companyURL: 'https://apps.shopify.com/pagefly',
+    companyBio: 'Top 3 Page Builder app on Shopify',
+    works: [
+      `Built Pagefly's Analytics APIs`,
+      <>
+        Built{' '}
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/sellersmith/slider-x">
+          Slider-x
+        </a>
+      </>,
+    ],
+  },
+  {
+    time: 'May 2017 - Jul 2018',
+    role: 'Coding Instructor and Developer',
+    company: 'MindX - Technology School',
+    companyURL: 'https://mindx.edu.vn/',
+    companyBio: '(aka Techkids)',
+    works: [
+      <>
+        Built{' '}
+        <a target="_blank" rel="noopener noreferrer" href="http://lok.vn">
+          Love of Knowledge
+        </a>{' '}
+        (Front-end part)
+      </>,
+      <>
+        Built{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://hta218.github.io/get-a-room-api-docs/"
+        >
+          Room Finder APIs
+        </a>
+      </>,
+      <>
+        Built{' '}
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/hta218/Travel_Egypt">
+          Travel Egypt
+        </a>{' '}
+        - a picture puzzle game made by pygame (Python)
+      </>,
+      'Tech Python fundamentals and Python web development',
+    ],
+  },
+  {
+    time: 'Sep 2013 - Feb 2019 (Move to Ha Noi city)',
+    role: 'Study Electronics and Telecommunications',
+    company: 'Hanoi University of Science and Technology',
+    companyURL: 'https://www.hust.edu.vn/',
+  },
+  {
+    time: 'Before 2013',
+    role: (
+      <>
+        Grown up in{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://en.wikipedia.org/wiki/M%E1%BB%99c_Ch%C3%A2u_District"
+        >
+          Moc Chau
+        </a>{' '}
+        District
+      </>
+    ),
+  },
+]
+
+module.exports = timelineData
