a45102db614e9a77f39e097a2afc2b8b026cd11b
Leo Huynh <huynhtuananh21895@gmail.com>
2021-12-22 23:23:33 +0700

    Optimize mobile-nav

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -49,7 +49,7 @@ const MobileNav = () => {
         </svg>
       </button>
       <div
-        className={`fixed w-full h-full top-0 right-0 bg-gray-200 dark:bg-gray-800 opacity-95 z-10 transition-transform transform ease-in-out duration-300 ${
+        className={`fixed w-full h-full inset-0 bg-gray-200 dark:bg-gray-800 opacity-95 z-10 transition-transform transform ease-in-out duration-300 ${
           navShow ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
