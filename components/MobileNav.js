20689f7565577dceafc3840efdf7171f6c675128
Leo Huynh <huynhtuananh21895@gmail.com>
2021-12-22 23:11:36 +0700

    Update mobile-nav

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -8,10 +8,12 @@ const MobileNav = () => {
   const onToggleNav = () => {
     setNavShow((status) => {
       if (status) {
-        document.body.style.overflow = 'auto'
+        document.documentElement.style.overflowY = 'auto'
+        document.body.style.overflowY = 'auto'
       } else {
         // Prevent scrolling
-        document.body.style.overflow = 'hidden'
+        document.documentElement.style.overflowY = 'hidden'
+        document.body.style.overflowY = 'hidden'
       }
       return !status
     })
@@ -47,22 +49,35 @@ const MobileNav = () => {
         </svg>
       </button>
       <div
-        className={`fixed w-full h-full top-24 right-0 bg-gray-200 dark:bg-gray-800 opacity-95 z-10 transition-transform transform ease-in-out duration-300 ${
+        className={`fixed w-full h-full top-0 right-0 bg-gray-200 dark:bg-gray-800 opacity-95 z-10 transition-transform transform ease-in-out duration-300 ${
           navShow ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
         <button
           type="button"
           aria-label="toggle modal"
-          className="fixed w-full h-full cursor-auto focus:outline-none"
+          className="fixed w-8 h-8 right-4 top-4 cursor-auto focus:outline-none"
           onClick={onToggleNav}
-        ></button>
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="text-gray-900 dark:text-gray-100"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
         <nav className="fixed h-full mt-8">
           {headerNavLinks.map((link) => (
-            <div key={link.title} className="px-12 py-4">
+            <div key={link.title} className="px-8 py-4">
               <Link
                 href={link.href}
-                className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
+                className="text-2xl font-semibold tracking-wide text-gray-900 dark:text-gray-100"
                 onClick={onToggleNav}
               >
                 {link.title}
