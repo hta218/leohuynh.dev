96686bea01f9bb6f392f520323c8d2200f3475c4
Leo Huynh <huynhtuananh21895@gmail.com>
2021-12-30 23:56:51 +0700

    Update layout

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,35 +1,31 @@
-import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Link from './Link'
-import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import NextImage from 'next/image'
+import { useState } from 'react'
 
 const LayoutWrapper = ({ children }) => {
+  const [navShow, setNavShow] = useState(false)
+  const onToggleNav = () => setNavShow((status) => !status)
+
   return (
-    <div className="max-w-3xl px-3 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
-      <div className="flex flex-col justify-between min-h-screen">
-        <header className="flex items-center justify-between py-5 md:py-10 sticky top-0 z-40 bg-white dark:bg-[#22272e]">
+    <>
+      <MobileNav navShow={navShow} onToggleNav={onToggleNav} />
+      <header className="overflow-x-hidden backdrop-blur supports-backdrop-blur:bg-white/95 py-3 sticky top-0 z-40 bg-white/75 dark:bg-dark/75">
+        <div className="mx-auto max-w-3xl xl:max-w-5xl flex items-center justify-between px-3 xl:px-0">
           <div>
             <Link href="/" aria-label="Leo's Blog">
               <div className="flex items-center justify-between">
                 <div className="mr-3 flex justify-center items-center">
                   <NextImage
                     src="/static/images/logo.jpg"
-                    width={50}
-                    height={50}
+                    width={40}
+                    height={40}
                     className="rounded-full"
                   />
                 </div>
-                {/* {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )} */}
               </div>
             </Link>
           </div>
@@ -46,13 +42,35 @@ const LayoutWrapper = ({ children }) => {
               ))}
             </div>
             <ThemeSwitch />
-            <MobileNav />
+            <button
+              className="w-8 h-8 ml-1 mr-1 rounded sm:hidden"
+              type="button"
+              aria-label="Toggle Menu"
+              onClick={onToggleNav}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="text-gray-900 dark:text-gray-100"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
           </div>
-        </header>
-        <main className="mb-auto">{children}</main>
-        <Footer />
+        </div>
+      </header>
+      <div className="max-w-3xl px-3 xl:px-0 mx-auto sm:px-6 xl:max-w-5xl">
+        <div className="flex flex-col justify-between min-h-screen">
+          <main className="mb-auto">{children}</main>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
