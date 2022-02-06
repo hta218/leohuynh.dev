c7eea50d5cb60b5fe8025173ff43e581d697d6c9
Leo Huynh <huynhtuananh21895@gmail.com>
2022-02-06 23:32:29 +0700

    Update Twemoji, add emoji for bios

--- components/Twemoji.js
+++ components/Twemoji.js
@@ -1,8 +1,8 @@
 import kebabCase from '@/lib/utils/kebabCase'
 
-const Twemoji = ({ emoji, size = 'twa-lg' }) => {
+const Twemoji = ({ emoji, size = 'twa-lg', className }) => {
   const twaClass = kebabCase(emoji)
-  return <i className={`twa ${size} twa-${twaClass}`} />
+  return <i className={`twa ${size} twa-${twaClass} ${className}`} />
 }
 
 export default Twemoji
