e8d0e7bed225c920a8a27e53c929d280e0f10404
Leo Huynh <huynhtuananh21895@gmail.com>
2022-03-06 15:18:47 +0700

    Update post layout

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -27,9 +27,12 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, pag
       <BlogSeo url={postUrl} authorDetails={authorDetails} {...frontMatter} />
       <ScrollTop />
       <article>
-        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+        <div className="">
           <header className="pt-6 xl:py-16">
             <div className="space-y-4 md:space-y-2 text-center">
+              <div>
+                <PageTitle>{title}</PageTitle>
+              </div>
               <dl className="space-y-10">
                 <div>
                   <dt className="sr-only">Published on</dt>
@@ -48,64 +51,90 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, pag
                   </dd>
                 </div>
               </dl>
-              <div>
-                <PageTitle>{title}</PageTitle>
-              </div>
             </div>
           </header>
           <div
             className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6"
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
-            <dl className="pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200 xl:dark:border-gray-700">
-              <dt className="sr-only">Authors</dt>
-              <dd>
-                <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
-                  {authorDetails.map((author) => (
-                    <li className="flex items-center space-x-2" key={author.name}>
-                      {author.avatar && (
-                        <Image
-                          src={author.avatar}
-                          width="38px"
-                          height="38px"
-                          alt="avatar"
-                          className="w-10 h-10 rounded-full"
-                        />
-                      )}
-                      <dl className="text-sm font-medium leading-5 whitespace-nowrap">
-                        <dt className="sr-only">Name</dt>
-                        <dd className="text-gray-900 dark:text-gray-100 mb-0.5">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.github && (
-                            <>
-                              <Link
-                                href={author.github}
-                                className="text-primary-500 dark:text-primary-400 hover:text-primary-700 dark:hover:text-primary-300"
-                              >
-                                {author.github.replace('https://github.com/', '@')}
-                              </Link>
-                            </>
-                          )}
-                        </dd>
-                      </dl>
-                    </li>
-                  ))}
-                </ul>
-              </dd>
-            </dl>
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
+            <div>
+              <div className="hidden xl:block py-4 xl:py-8 border-b border-gray-200 dark:border-gray-700">
+                <Link
+                  href={`/blog/page/${page}`}
+                  className="flex text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                >
+                  <svg viewBox="0 -9 3 24" className="overflow-visible mr-3 w-auto h-6">
+                    <path
+                      d="M3 0L0 3L3 6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </svg>
+                  Back to the blog
+                </Link>
+              </div>
+              <dl className="pt-6 pb-10 xl:pt-11">
+                <dt className="sr-only">Authors</dt>
+                <dd>
+                  <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-6">
+                    {authorDetails.map((author) => (
+                      <li className="flex items-center space-x-2" key={author.name}>
+                        {author.avatar && (
+                          <Image
+                            src={author.avatar}
+                            width="38px"
+                            height="38px"
+                            alt="avatar"
+                            className="w-10 h-10 rounded-full"
+                          />
+                        )}
+                        <dl className="text-sm font-medium leading-5 whitespace-nowrap">
+                          <dt className="sr-only">Name</dt>
+                          <dd className="text-gray-900 dark:text-gray-100 mb-0.5">{author.name}</dd>
+                          <dt className="sr-only">Twitter</dt>
+                          <dd>
+                            {author.github && (
+                              <>
+                                <Link
+                                  href={author.github}
+                                  className="text-primary-500 dark:text-primary-400 hover:text-primary-700 dark:hover:text-primary-300"
+                                >
+                                  {author.github.replace('https://github.com/', '@')}
+                                </Link>
+                              </>
+                            )}
+                          </dd>
+                        </dl>
+                      </li>
+                    ))}
+                  </ul>
+                </dd>
+              </dl>
+            </div>
+            <div className="!border-t-0 divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
               <div className="pt-10 pb-8 prose prose-lg dark:prose-dark max-w-none">{children}</div>
               <SocialButtons postUrl={postUrl} title={title} fileName={fileName} />
               <Comments frontMatter={frontMatter} />
             </div>
             <footer>
-              <div className="text-sm font-medium leading-5 divide-gray-200 xl:divide-y dark:divide-gray-700 xl:col-start-1 xl:row-start-2">
+              <div className="text-sm font-medium leading-5 xl:col-start-1 xl:row-start-2">
+                {/* <div className="pt-4 xl:pt-8">
+                  <Link
+                    href={`/blog/page/${page}`}
+                    className="flex text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                  >
+                    <svg viewBox="0 -9 3 24" class="overflow-visible mr-3 w-auto h-6"><path d="M3 0L0 3L3 6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    Back to the blog
+                  </Link>
+                </div> */}
                 {tags && (
-                  <div className="py-4 xl:py-8">
-                    <h2 className="text-sm tracking-wide text-gray-500 dark:text-gray-400 mb-1">
+                  <div className="py-4">
+                    {/* <h2 className="text-sm tracking-wide text-gray-500 dark:text-gray-400 mb-1">
                       Tags
-                    </h2>
+                    </h2> */}
                     <div className="flex flex-wrap">
                       {tags.map((tag) => (
                         <Tag key={tag} text={tag} />
@@ -113,7 +142,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, pag
                     </div>
                   </div>
                 )}
-                {(next || prev) && (
+                {/* {(next || prev) && (
                   <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
                     {prev && (
                       <div>
@@ -140,15 +169,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, pag
                       </div>
                     )}
                   </div>
-                )}
-              </div>
-              <div className="pt-4 xl:pt-8">
-                <Link
-                  href={`/blog/page/${page}`}
-                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                >
-                  &larr; Back to the blog
-                </Link>
+                )} */}
               </div>
             </footer>
           </div>
