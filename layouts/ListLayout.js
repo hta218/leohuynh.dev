b370897cdb06581dfe2b31a3a407334eb123b0e8
Leo Huynh <huynhtuananh21895@gmail.com>
2022-06-19 16:36:12 +0700

    style: mobile style

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -18,7 +18,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
   return (
     <>
       <div className="divide-y">
-        <div className="pt-6 pb-12 space-y-2 md:space-y-5">
+        <div className="pt-6 pb-12 space-y-4 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
