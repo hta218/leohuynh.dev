96c788f42ba160d5987d36f8122af16d814f58bb
Leo Huynh <huynhtuananh21895@gmail.com>
2022-06-19 13:57:13 +0700

    Update twemoji, add more emoji

--- components/Twemoji.js
+++ components/Twemoji.js
@@ -1,7 +1,7 @@
 import kebabCase from '@/lib/utils/kebabCase'
 
 const Twemoji = ({ emoji, size = 'twa-lg', className }) => {
-  let cls = `twa ${size} twa-${kebabCase(emoji)} ${className || ''}`
+  let cls = `inline-block twa ${size} twa-${kebabCase(emoji)} ${className || ''}`
   return <i className={cls.trim()} />
 }
 
