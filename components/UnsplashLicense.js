7b9609a1f98a0c0ccb1f6fae8c6f770df17b2475
Leo Huynh <huynhtuananh21895@gmail.com>
2022-06-19 12:38:34 +0700

    Update Unsplash comp

--- components/UnsplashLicense.js
+++ components/UnsplashLicense.js
@@ -1,6 +1,6 @@
 const UnsplashLicense = ({ photoURL, author }) => {
   return (
-    <div className="text-right italic text-sm my-2">
+    <div className="text-right italic text-sm mt-4 mb-12">
       Photo by{' '}
       <a className="!no-underline" href={photoURL} target="_blank" rel="noreferrer">
         {author}
