1bdfc292b4022f0d37f7464f97ae08b71b431430
Leo Huynh <huynhtuananh21895@gmail.com>
2022-06-19 11:03:17 +0700

    Fix Twemoji element class

--- components/Twemoji.js
+++ components/Twemoji.js
@@ -1,8 +1,8 @@
 import kebabCase from '@/lib/utils/kebabCase'
 
 const Twemoji = ({ emoji, size = 'twa-lg', className }) => {
-  const twaClass = kebabCase(emoji)
-  return <i className={`twa ${size} twa-${twaClass} ${className}`} />
+  let cls = `twa ${size} twa-${kebabCase(emoji)} ${className || ''}`
+  return <i className={cls.trim()} />
 }
 
 export default Twemoji
