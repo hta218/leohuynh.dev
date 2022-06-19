2d1a14ae767b3d06b8026635e985e5b481bf040d
Leo Huynh <huynhtuananh21895@gmail.com>
2022-06-19 15:01:48 +0700

    style: post style

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           ></link>
           <link
