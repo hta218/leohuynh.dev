fe19be88dc002b332afc3b2a603ac30a9ea5c86f
Leo Huynh <huynhtuananh21895@gmail.com>
2022-06-19 11:47:23 +0700

    Add credit to footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import SocialIcon from '@/components/social-icons'
 export default function Footer() {
   return (
     <footer>
-      <div className="flex flex-col items-center mt-16">
+      <div className="flex flex-col items-center mt-16 mb-8">
         <div className="flex mb-3 space-x-4">
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
@@ -14,15 +14,21 @@ export default function Footer() {
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
         </div>
-        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="flex my-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{`Copyright © ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.footerTitle}</Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            {/* Tailwind Nextjs Theme */}
-          </Link>
+        <div className="text-sm text-gray-500 dark:text-gray-400">
+          <span className="font-bold text-gray-500 dark:text-gray-400">Credit : </span>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-500"
+            href="https://github.com/timlrx/tailwind-nextjs-starter-blog"
+          >
+            Tailwind Nextjs Theme
+          </a>
         </div>
       </div>
     </footer>
