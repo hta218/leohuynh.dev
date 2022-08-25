d2c8b4efe6e2db011f105e9cca4513200be23b56
hta218 <huynhtuananh21895@gmail.com>
2022-08-25 20:31:54 +0700

    feat: add mdx utils

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -6,11 +6,11 @@ const siteMetadata = require('../data/siteMetadata')
 ;(async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
   const pages = await globby([
-    'pages/*.js',
+    'pages/*.ts',
     'data/blog/**/*.mdx',
     'data/blog/**/*.md',
     'public/tags/**/*.xml',
-    '!pages/_*.js',
+    '!pages/_*.ts',
     '!pages/api',
   ])
 
@@ -23,12 +23,12 @@ const siteMetadata = require('../data/siteMetadata')
                   .replace('pages/', '/')
                   .replace('data/blog', '/blog')
                   .replace('public/', '/')
-                  .replace('.js', '')
+                  .replace('.ts', '')
                   .replace('.mdx', '')
                   .replace('.md', '')
                   .replace('/feed.xml', '')
                 const route = path === '/index' ? '' : path
-                if (page === `pages/404.js` || page === `pages/blog/[...slug].js`) {
+                if (page === `pages/404.ts` || page === `pages/blog/[...slug].ts`) {
                   return
                 }
                 return `
