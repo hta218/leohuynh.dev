f2e4dca7316e4360952a4e03f5fe279aa165706c
hta218 <huynhtuananh21895@gmail.com>
2022-08-26 18:09:19 +0700

    feat: update generate sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,51 +1,50 @@
-const fs = require('fs')
-const globby = require('globby')
-const prettier = require('prettier')
-const siteMetadata = require('../data/siteMetadata')
+let fs = require('fs')
+let globby = require('globby')
+let prettier = require('prettier')
+
+let SITE_URL = 'https://www.leohuynh.dev'
 
 ;(async () => {
-  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
-  const pages = await globby([
-    'pages/*.ts',
+  let prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
+  let pages = await globby([
+    'pages/*.tsx',
     'data/blog/**/*.mdx',
     'data/blog/**/*.md',
     'public/tags/**/*.xml',
-    '!pages/_*.ts',
+    '!pages/_*.tsx',
     '!pages/api',
   ])
 
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map((page) => {
-                const path = page
-                  .replace('pages/', '/')
-                  .replace('data/blog', '/blog')
-                  .replace('public/', '/')
-                  .replace('.ts', '')
-                  .replace('.mdx', '')
-                  .replace('.md', '')
-                  .replace('/feed.xml', '')
-                const route = path === '/index' ? '' : path
-                if (page === `pages/404.ts` || page === `pages/blog/[...slug].ts`) {
-                  return
-                }
-                return `
-                        <url>
-                            <loc>${siteMetadata.siteUrl}${route}</loc>
-                        </url>
-                    `
-              })
-              .join('')}
-        </urlset>
+  let sitemap = `
+			<?xml version="1.0" encoding="UTF-8"?>
+			<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+				${pages
+          .map((page) => {
+            let path = page
+              .replace('pages/', '/')
+              .replace('data/blog', '/blog')
+              .replace('public/', '/')
+              .replace('.ts', '')
+              .replace('.mdx', '')
+              .replace('.md', '')
+              .replace('/feed.xml', '')
+            let route = path === '/index' ? '' : path
+            if (page === `pages/404.ts` || page === `pages/blog/[...slug].ts`) {
+              return
+            }
+            return `<url><loc>${SITE_URL}${route}</loc></url>`
+          })
+          .join('')}
+			</urlset>
     `
 
-  const formatted = prettier.format(sitemap, {
+  let formatted = prettier.format(sitemap, {
     ...prettierConfig,
     parser: 'html',
   })
 
   // eslint-disable-next-line no-sync
   fs.writeFileSync('public/sitemap.xml', formatted)
+
+  console.log('Sitemap generated successfully in `public/sitemap.xml`.')
 })()
