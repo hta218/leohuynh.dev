9ebf2a1dffa76d2eef8279c4e59517d4f9fc8bd0
hta218 <huynhtuananh21895@gmail.com>
2022-08-26 12:12:41 +0700

    feat: add constants

---
+++ constant/index.ts
@@ -0,0 +1,12 @@
+export let BLUR_IMAGE_DATA_URL =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNcvWS1LgAGJQIpt50GkgAAAABJRU5ErkJggg=='
+
+export let LOGO_IMAGE_PATH = '/static/images/logo.jpg'
+
+export let GISCUS_COMMENTS_ID = 'comments-container'
+export let UTTERANCES_COMMENTs_ID = 'comments-container'
+export let DISQUS_COMMENTS_ID = 'disqus_thread'
+
+export let HEADER_HEIGHT = '69px'
+export let FOOTER_HEIGHT = '188px'
+export let MAIN_CONTENT_MIN_HEIGHT = `calc(100vh - ${HEADER_HEIGHT} - ${FOOTER_HEIGHT})`
