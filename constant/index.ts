bd8d5fa599a2a5bf1f352f3bccce4d05d5e8860e
hta218 <huynhtuananh21895@gmail.com>
2022-08-26 16:53:39 +0700

    feat: add blog constants

--- constant/index.ts
+++ constant/index.ts
@@ -10,3 +10,24 @@ export let DISQUS_COMMENTS_ID = 'disqus_thread'
 export let HEADER_HEIGHT = '69px'
 export let FOOTER_HEIGHT = '188px'
 export let MAIN_CONTENT_MIN_HEIGHT = `calc(100vh - ${HEADER_HEIGHT} - ${FOOTER_HEIGHT})`
+
+export let SPOTIFY_TOKEN_API = `https://accounts.spotify.com/api/token`
+export let SPOTIFY_NOW_PLAYING_API = `https://api.spotify.com/v1/me/player/currently-playing`
+export let SPOTIFY_TOP_TRACKS_API = `https://api.spotify.com/v1/me/top/tracks`
+
+export let TOKEN_CLASSNAME_MAP = {
+  tag: 'text-code-red',
+  'attr-name': 'text-code-yellow',
+  'attr-value': 'text-code-green',
+  deleted: 'text-code-red',
+  inserted: 'text-code-green',
+  punctuation: 'text-code-white',
+  keyword: 'text-code-purple',
+  string: 'text-code-green',
+  function: 'text-code-blue',
+  boolean: 'text-code-red',
+  comment: 'text-gray-500 italic',
+}
+
+export let POSTS_PER_PAGE = 5
+export let FEATURED_POSTS = 5
