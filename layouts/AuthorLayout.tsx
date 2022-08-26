26d2b1b136661827b10039ac9431c5cea08afe25
hta218 <huynhtuananh21895@gmail.com>
2022-08-26 15:49:08 +0700

    refactor: simplify Layouts

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -1,20 +1,24 @@
-import ProfileCard from 'components/ProfileCard'
-import { PageSeo } from 'components/SEO'
+import React from 'react'
+import { PageSeo, ProfileCard } from '~/components'
+import { siteMetadata } from '~/data'
+import type { AuthorLayoutProps } from '~/types'
 
-export default function AuthorLayout({ children, frontMatter }) {
-  const { name } = frontMatter
+export default function AuthorLayout({ children }: AuthorLayoutProps) {
+  let title = 'About me'
+  let description = 'More about me and myself'
 
   return (
     <>
-      <PageSeo title={`About - ${name}`} description={`About me - ${name}`} />
+      <PageSeo
+        title={`${title} - ${siteMetadata.author} - ${siteMetadata.title}`}
+        description={`${title} - ${siteMetadata.title} - ${description}`}
+      />
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            {title}
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            More about me and myself
-          </p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">{description}</p>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:space-y-0 pt-8">
           <ProfileCard />
