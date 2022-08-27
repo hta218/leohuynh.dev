bc93340a63e367e1c7bde2b355c6b90ccf23893e
hta218 <huynhtuananh21895@gmail.com>
2022-08-27 16:02:13 +0700

    chore: clean

--- utils/kebabCase.ts
+++ utils/kebabCase.ts
@@ -1,4 +1,4 @@
-export function kebabCase(str: string) {
+export function kebabCase(str: string): string {
   return str
     ?.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
     ?.map((x) => x.toLowerCase())
