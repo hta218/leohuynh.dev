2c82213501d6fe96d7027826fee576b77d11e199
hta218 <huynhtuananh21895@gmail.com>
2022-08-27 22:15:38 +0700

    fix: using react in production instead of preact

--- next.config.js
+++ next.config.js
@@ -31,15 +31,15 @@ module.exports = withBundleAnalyzer({
       use: ['@svgr/webpack'],
     })
 
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
+    // if (!dev && !isServer) {
+    // 	// Replace React with Preact only in client production build
+    // 	Object.assign(config.resolve.alias, {
+    // 		'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+    // 		react: 'preact/compat',
+    // 		'react-dom/test-utils': 'preact/test-utils',
+    // 		'react-dom': 'preact/compat',
+    // 	})
+    // }
 
     return config
   },
