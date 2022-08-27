3614d4eafbe32c81f1273dd3077979bbd800c57a
hta218 <huynhtuananh21895@gmail.com>
2022-08-27 15:56:38 +0700

    feat: add seo types

---
+++ types/seo.ts
@@ -0,0 +1,14 @@
+export type PageSeoProps = {
+  title: string
+  description: string
+}
+
+export type BlogSeoProps = {
+  authorDetails: any
+  title: string
+  summary: string
+  date: string
+  lastmod: string
+  url: string
+  images: any[]
+}
