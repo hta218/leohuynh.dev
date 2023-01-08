161a6141b91d3b70ce210280ba8be0e3757a57d0
hta218 <huynhtuananh21895@gmail.com>
2023-01-08 15:32:48 +0700

    fix(eslint): automatically sorts classes

--- components/AnalyticsLink.tsx
+++ components/AnalyticsLink.tsx
@@ -5,7 +5,7 @@ export function AnalyticsLink() {
     <button
       aria-label="Open analytics"
       type="button"
-      className="p-2 ml-1 rounded sm:ml-2 hover:bg-gray-200 dark:hover:bg-gray-700 umami--click--nav-analytics"
+      className="umami--click--nav-analytics ml-1 rounded p-2 hover:bg-gray-200 dark:hover:bg-gray-700 sm:ml-2"
       onClick={() => window.open(siteMetadata.analyticsURL, '_blank')}
     >
       <svg
@@ -13,7 +13,7 @@ export function AnalyticsLink() {
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
-        className="text-gray-900 dark:text-gray-100 w-5 h-5"
+        className="h-5 w-5 text-gray-900 dark:text-gray-100"
       >
         <path d="M1.019 13.019h3.849V24h-3.85zm8.943-6.68h3.85V24h-3.85zM19.132 0h3.85v24h-3.85z" />
       </svg>
