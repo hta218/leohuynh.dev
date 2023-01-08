b0d371f7de8bf1f51004060ed53405f4bf35554d
hta218 <huynhtuananh21895@gmail.com>
2023-01-08 12:06:58 +0700

    feat: update project data

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,47 +1,58 @@
-import type { ProjectCardProps } from '~/types'
+import type { GithubRepository, ProjectCardProps } from '~/types'
+import { fetcher } from '~/utils/fetcher'
+import { GithubRepo } from './GithubRepo'
 import { Image } from './Image'
 import { Link } from './Link'
+const { default: useSWR } = require('swr')
 
 export function ProjectCard({ project }: ProjectCardProps) {
-  let { title, description, imgSrc, href, repoName } = project
+  let { title, description, imgSrc, url, repo, builtWith } = project
+  let { data } = useSWR(`/api/github?repo=${repo}`, fetcher)
+  let repository: GithubRepository = data?.repository
 
   return (
-    <div className="p-4 md:w-1/2 md" style={{ maxWidth: '544px' }}>
-      <div className="h-full overflow-hidden border dark:hover:border-gray-400 dark:border-gray-600 border-gray-400 hover:border-gray-500 rounded-md border-opacity-60">
+    <div className="md p-4 md:w-1/2" style={{ maxWidth: '544px' }}>
+      <div className="flex h-full flex-col overflow-hidden rounded-md border border-gray-400 border-opacity-60 hover:border-gray-500 dark:border-gray-600 dark:hover:border-gray-400">
         <Image
           alt={title}
           src={imgSrc}
-          className="object-cover object-center lg:h-48 md:h-36"
+          className="object-cover object-top md:h-36 lg:h-60"
           width={1088}
           height={612}
         />
-        <div className="p-6">
-          <div className="flex justify-between mb-3">
+        <div className="flex grow flex-col justify-between space-y-8 p-6">
+          <div className="space-y-3">
             <h2 className="text-2xl font-bold leading-8 tracking-tight">
-              {href ? (
-                <Link href={href} aria-label={`Link to ${title}`}>
+              {url ? (
+                <Link href={url} aria-label={`Link to ${title}`}>
                   {title}
                 </Link>
               ) : (
                 title
               )}
             </h2>
-            {/* {!repoName ? null : (
-              <div className="flex-shrink-0 mt-0.5">
-                <Image
-                  alt={title}
-                  src={`https://img.shields.io/github/stars/hta218/${repoName}.svg?style=social&label=Stars`}
-                  shouldOpenLightbox={false}
-                  width={112}
-                  height={28}
-                />
+            <div className="max-w-none space-y-1 text-gray-500 dark:text-gray-400">
+              <p>{repository?.description || description}</p>
+              <div className="flex space-x-2">
+                <span>Built with:</span>
+                <div className="flex space-x-1">
+                  {builtWith?.map((tool, index) => {
+                    return (
+                      <span key={index} className="font-semibold text-gray-500 dark:text-gray-400">
+                        {tool}
+                        {index !== builtWith.length - 1 && ','}
+                      </span>
+                    )
+                  })}
+                </div>
               </div>
-            )} */}
+            </div>
           </div>
-          <p className="mb-3 prose text-gray-500 max-w-none dark:text-gray-400">{description}</p>
-          {href && (
+          {repository ? (
+            <GithubRepo repo={repository} />
+          ) : (
             <Link
-              href={href}
+              href={url}
               className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
               aria-label={`Link to ${title}`}
             >
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -12,27 +12,27 @@ export default function Projects() {
     <>
       <PageSeo title={`Projects - ${siteMetadata.author}`} description={description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">{description}</p>
         </div>
         <div className="container py-12">
-          <h3 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 mb-4">
+          <h3 className="mb-4 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100">
             Work
           </h3>
-          <div className="flex flex-wrap -m-4">
+          <div className="-m-4 flex flex-wrap">
             {workProjects.map((project) => (
               <ProjectCard key={project.title} project={project} />
             ))}
           </div>
         </div>
         <div className="container py-12">
-          <h3 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 mb-4">
-            Side Projects
+          <h3 className="mb-4 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100">
+            Side projects
           </h3>
-          <div className="flex flex-wrap -m-4">
+          <div className="-m-4 flex flex-wrap">
             {sideProjects.map((project) => (
               <ProjectCard key={project.title} project={project} />
             ))}
