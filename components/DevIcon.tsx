5db8a1f99af9039457c24a867cf73430e83bca47
hta218 <huynhtuananh21895@gmail.com>
2023-01-08 18:05:03 +0700

    feat(dev-icons): allow passing custom size

--- components/DevIcon.tsx
+++ components/DevIcon.tsx
@@ -6,6 +6,7 @@ import Node from '~/icons/nodejs.svg'
 import Bash from '~/icons/bash.svg'
 import Liquid from '~/icons/liquid.svg'
 import Markdown from '~/icons/markdown.svg'
+import NextJS from '~/icons/nextjs.svg'
 
 export let DevIconsMap = {
   React,
@@ -16,11 +17,14 @@ export let DevIconsMap = {
   Bash,
   Liquid,
   Markdown,
+  NextJS,
 }
 
-export function DevIcon({ type }: { type: keyof typeof DevIconsMap }) {
+export function DevIcon(props: { type: keyof typeof DevIconsMap; className?: string }) {
+  let { type, className } = props
   let Icon = DevIconsMap[type]
   if (!Icon) return <div>Missing icon</div>
 
-  return <Icon className="h-16 w-16 lg:h-14 lg:w-14 xl:h-24 xl:w-24" fill="currentColor" />
+  let defaultClass = 'h-16 w-16 lg:h-14 lg:w-14 xl:h-24 xl:w-24'
+  return <Icon className={className || defaultClass} fill="currentColor" />
 }
