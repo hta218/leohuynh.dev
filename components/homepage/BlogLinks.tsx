aaf2e21b04574ede51deb721cceee79f44ad01be
hta218 <huynhtuananh21895@gmail.com>
2023-01-10 23:08:44 +0700

    style: update `BlogLinks`

--- components/homepage/BlogLinks.tsx
+++ components/homepage/BlogLinks.tsx
@@ -4,30 +4,30 @@ import { Twemoji } from '../Twemoji'
 
 export function BlogLinks() {
   return (
-    <div className="flex flex-col space-y-1">
-      <Link href="/projects" className="hover:underline">
+    <div className="flex flex-col space-y-2">
+      <Link href="/projects" className="flex hover:underline">
         <Twemoji emoji="hammer-and-wrench" />
-        <span className="umami--click--home-link-projects ml-2">What have I built?</span>
+        <span className="umami--click--home-link-projects ml-2.5">What have I built?</span>
       </Link>
-      <Link href="/blog" className="hover:underline">
+      <Link href="/blog" className="flex hover:underline">
         <Twemoji emoji="memo" />
-        <span className="umami--click--home-link-blog ml-2">My writings</span>
+        <span className="umami--click--home-link-blog ml-2.5">My writings</span>
       </Link>
-      <Link href="/snippets" className="hover:underline">
+      <Link href="/snippets" className="flex hover:underline">
         <Twemoji emoji="dna" />
-        <span className="umami--click--home-link-snippets ml-2">My snippets collection</span>
+        <span className="umami--click--home-link-snippets ml-2.5">My snippets collection</span>
       </Link>
-      <Link href="/about" className="hover:underline">
+      <Link href="/about" className="flex hover:underline">
         <Twemoji emoji="face-with-monocle" />
-        <span className="umami--click--home-link-about ml-2">More about me and myself</span>
+        <span className="umami--click--home-link-about ml-2.5">More about me and myself</span>
       </Link>
-      <Link href="/resume" className="hover:underline">
+      <Link href="/resume" className="flex hover:underline">
         <Twemoji emoji="briefcase" />
-        <span className="umami--click--home-link-resume ml-2">My career</span>
+        <span className="umami--click--home-link-resume ml-2.5">My career</span>
       </Link>
-      <Link href={siteMetadata.analyticsURL} className="hover:underline">
+      <Link href={siteMetadata.analyticsURL} className="flex hover:underline">
         <Twemoji emoji="bar-chart" />
-        <span className="umami--click--home-link-analytics ml-2">
+        <span className="umami--click--home-link-analytics ml-2.5">
           Traffic & engagement of this site
         </span>
       </Link>
