9411d4e755cc95de0d870a2f1e1dd1698f4a4a7d
hta218 <huynhtuananh21895@gmail.com>
2023-01-11 21:03:17 +0700

    style: update greating heading

--- components/homepage/Greeting.tsx
+++ components/homepage/Greeting.tsx
@@ -1,13 +1,13 @@
-export function Greeting() {
-  let headingColorClass =
-    'bg-gradient-to-r from-yellow-600 to-red-600 dark:bg-gradient-to-l dark:from-emerald-500 dark:to-lime-600'
+import clsx from 'clsx'
 
+export function Greeting() {
+  let className = clsx(
+    'bg-gradient-to-r from-yellow-600 to-red-600 dark:bg-gradient-to-l dark:from-emerald-500 dark:to-lime-600',
+    'mb-8 bg-clip-text text-4xl font-extrabold leading-[60px] tracking-tight text-transparent md:text-7xl md:leading-[86px]'
+  )
   return (
-    <div
-      className={`mb-8 bg-clip-text text-4xl font-extrabold leading-[60px] tracking-tight text-transparent ${headingColorClass} md:text-7xl md:leading-[86px]`}
-    >
-      Howdy, fellow!
-      {/* Howdy, fellow! <i className="twa twa-waving-hand"></i> */}
+    <div className={className}>
+      Howdy, fellow! <i className="twa twa-waving-hand"></i>
     </div>
   )
 }
