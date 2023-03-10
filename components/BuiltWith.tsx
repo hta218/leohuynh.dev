78293d643459b3c0c755ef1b70de5c193afa5b67
hta218 <huynhtuananh21895@gmail.com>
2023-03-10 20:36:03 +0700

    chore: add `ref` in built with links

--- components/BuiltWith.tsx
+++ components/BuiltWith.tsx
@@ -7,19 +7,19 @@ export function BuiltWith() {
     <div className="flex items-center space-x-1">
       <span className="mr-1 text-gray-500 dark:text-gray-400">Built with</span>
       <div className="flex space-x-1.5">
-        <Link href="https://nextjs.org/">
+        <Link href="https://nextjs.org?ref=leohuynh.dev">
           <DevIcon type="NextJS" className="h-5 w-5" />
         </Link>
-        <Link href="https://tailwindcss.com/">
+        <Link href="https://tailwindcss.com?ref=leohuynh.dev">
           <DevIcon type="TailwindCSS" className="h-5 w-5" />
         </Link>
-        <Link href="https://www.prisma.io/">
+        <Link href="https://www.prisma.io?ref=leohuynh.dev">
           <DevIcon type="Prisma" className="h-5 w-5" />
         </Link>
-        <Link href="https://www.typescriptlang.org/">
+        <Link href="https://www.typescriptlang.org?ref=leohuynh.dev">
           <DevIcon type="Typescript" className="h-5 w-5" />
         </Link>
-        <Link href="https://umami.is/" className="pl-px">
+        <Link href="https://umami.is?ref=leohuynh.dev" className="pl-px">
           <DevIcon type="Umami" className="h-5 w-5" />
         </Link>
       </div>
