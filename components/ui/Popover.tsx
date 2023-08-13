f9c3ad9ab8dd6e9cffb154d0003214355a417bb5
hta218 <huynhtuananh21895@gmail.com>
2023-08-13 18:34:51 +0700

    feat: add `Popover` ui component

---
+++ components/ui/Popover.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import * as PopoverPrimitive from '@radix-ui/react-popover'
+import { clsx } from 'clsx'
+
+let Popover = PopoverPrimitive.Root
+
+let PopoverTrigger = PopoverPrimitive.Trigger
+
+let PopoverContent = React.forwardRef<
+  React.ElementRef<typeof PopoverPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
+>(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
+  <PopoverPrimitive.Portal>
+    <PopoverPrimitive.Content
+      ref={ref}
+      align={align}
+      sideOffset={sideOffset}
+      className={clsx(
+        'z-50 rounded bg-popover p-1.5 text-popover-foreground shadow-popover dark:shadow-popover-dark outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        className
+      )}
+      {...props}
+    />
+  </PopoverPrimitive.Portal>
+))
+PopoverContent.displayName = PopoverPrimitive.Content.displayName
+
+export { Popover, PopoverTrigger, PopoverContent }
