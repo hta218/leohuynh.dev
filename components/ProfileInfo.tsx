711902eeebec3b7e841288172cf74892a6fa7b43
hta218 <huynhtuananh21895@gmail.com>
2023-08-14 16:57:58 +0700

    chore: typo

--- components/ProfileInfo.tsx
+++ components/ProfileInfo.tsx
@@ -15,7 +15,7 @@ export function ProfileCardInfo() {
         <div className="flex items-center text-gray-700 dark:text-gray-200">
           <Backpack strokeWidth={1} size={20} />
           <p className="flex items-center px-2 space-x-1">
-            <span>CTO / Co.Founder</span>
+            <span>CTO & Co-Founder</span>
             <span>@</span>
             <a
               target="_blank"
