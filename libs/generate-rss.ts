1951065d18bae7d6cbbbe92a8000c1ab7e352e00
hta218 <huynhtuananh21895@gmail.com>
2023-08-14 15:52:02 +0700

    refactor: update rss

--- libs/generate-rss.ts
+++ libs/generate-rss.ts
@@ -2,39 +2,33 @@ import { escape } from '~/utils/html-escaper'
 import type { BlogFrontMatter } from '~/types/mdx'
 import type { SiteMetaData } from '~/types/data'
 
-function generateRssItem(lang_siteMetadata: SiteMetaData, post: BlogFrontMatter) {
+function generateRssItem(metadata: SiteMetaData, post: BlogFrontMatter) {
   return `
     <item>
-      <guid>${lang_siteMetadata.siteUrl}/blog/${post.slug}</guid>
+      <guid>${metadata.siteUrl}/blog/${post.slug}</guid>
       <title>${escape(post.title)}</title>
-      <link>${lang_siteMetadata.siteUrl}/blog/${post.slug}</link>
+      <link>${metadata.siteUrl}/blog/${post.slug}</link>
       ${post.summary && `<description>${escape(post.summary)}</description>`}
       <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-      <author>${lang_siteMetadata.email} (${lang_siteMetadata.author})</author>
+      <author>${metadata.email} (${metadata.author})</author>
       ${post.tags && post.tags.map((t) => `<category>${t}</category>`).join('')}
     </item>
   `
 }
 
-export function generateRss(
-  lang_siteMetadata: SiteMetaData,
-  posts: BlogFrontMatter[],
-  page = 'feed.xml'
-) {
+export function generateRss(metadata: SiteMetaData, posts: BlogFrontMatter[], page = 'feed.xml') {
   return `
     <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
-        <title>${escape(lang_siteMetadata.title)}</title>
-        <link>${lang_siteMetadata.siteUrl}/blog</link>
-        <description>${escape(lang_siteMetadata.description)}</description>
-        <language>${lang_siteMetadata.language}</language>
-        <managingEditor>${lang_siteMetadata.email} (${lang_siteMetadata.author})</managingEditor>
-        <webMaster>${lang_siteMetadata.email} (${lang_siteMetadata.author})</webMaster>
+        <title>${escape(metadata.title)}</title>
+        <link>${metadata.siteUrl}/blog</link>
+        <description>${escape(metadata.description)}</description>
+        <language>${metadata.language}</language>
+        <managingEditor>${metadata.email} (${metadata.author})</managingEditor>
+        <webMaster>${metadata.email} (${metadata.author})</webMaster>
         <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
-        <atom:link href="${
-          lang_siteMetadata.siteUrl
-        }/${page}" rel="self" type="application/rss+xml"/>
-        ${posts.map((post) => generateRssItem(lang_siteMetadata, post)).join('')}
+        <atom:link href="${metadata.siteUrl}/${page}" rel="self" type="application/rss+xml"/>
+        ${posts.map((post) => generateRssItem(metadata, post)).join('')}
       </channel>
     </rss>
   `
