febfd31298c8cc11eb51645b6ed1fc63e1bc5ad8
hta218 <huynhtuananh21895@gmail.com>
2023-09-26 16:30:56 +0700

    fix: wrong link on tags list page

--- components/PostListItem.tsx
+++ components/PostListItem.tsx
@@ -1,13 +1,21 @@
 import { useTranslation } from 'next-i18next'
-import type { MdxFrontMatter } from '~/types/mdx'
+import type { BlogFrontMatter, SnippetFrontMatter } from '~/types/mdx'
 import { formatDate } from '~/utils/date'
 import { Link } from './Link'
 import { Tag } from './Tag'
 
-export function PostListItem({ frontMatter }: { frontMatter: MdxFrontMatter }) {
-  let { slug, date, title, summary, tags } = frontMatter
+export function PostListItem({
+  frontMatter,
+}: {
+  frontMatter: BlogFrontMatter | SnippetFrontMatter
+}) {
+  let { slug, date, title, summary, tags, heading, type } = frontMatter as BlogFrontMatter &
+    SnippetFrontMatter
   let { t, i18n } = useTranslation()
   let lang = i18n.language
+  let isSnippets = heading && type
+  let category = isSnippets ? 'snippets' : 'blog'
+
   return (
     <li key={slug}>
       <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
@@ -20,7 +28,7 @@ export function PostListItem({ frontMatter }: { frontMatter: MdxFrontMatter }) {
         <div className="space-y-3 xl:col-span-3">
           <div className="space-y-3">
             <h3 className="text-2xl font-bold leading-8 tracking-tight">
-              <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+              <Link href={`/${category}/${slug}`} className="text-gray-900 dark:text-gray-100">
                 <span data-umami-event="blog-title">{title}</span>
               </Link>
             </h3>
