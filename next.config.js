a8ac8713a38ec4a7a4e28c0f4dbcf25265754aff
hta218 <huynhtuananh21895@gmail.com>
2024-04-01 22:47:59 +0700

    chore: update `next.config.js` to use `remotePatterns` for image domains

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = withBundleAnalyzer({
     dirs: ['components', 'constant', 'layouts', 'libs', 'pages', 'scripts', 'utils'],
   },
   images: {
-    domains: ['i.scdn.co'],
+    remotePatterns: [{ hostname: 'i.scdn.co' }],
   },
   typescript: { tsconfigPath: './tsconfig.json' },
   i18n,
