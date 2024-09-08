acfc2e0a8514e28fd7b0fb6fbf6e1d80d51fc0aa
hta218 <huynhtuananh21895@gmail.com>
2024-09-08 14:53:01 +0700

    Update headerNavLinks to include Snippets and Resume pages

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  { href: '/snippets', title: 'Snippets' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/resume', title: 'Resume' },
 ]
 
 export default headerNavLinks
