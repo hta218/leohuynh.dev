f4ec27fe1f8621a95f57d9e168a79fd0fa58d299
hta218 <huynhtuananh21895@gmail.com>
2024-09-10 21:37:37 +0700

    refactor: update social media links in ProfileInfo component

--- components/ProfileInfo.tsx
+++ components/ProfileInfo.tsx
@@ -1,7 +1,40 @@
 import { Backpack, Github, Linkedin, Mail, MapPin } from 'lucide-react'
 import Twemoji from './Twemoji'
 import siteMetadata from '@/data/siteMetadata'
-// import XIcon from '~/icons/x.svg'
+import XIcon from '~/icons/x.svg'
+import { Fragment } from 'react'
+
+function getPlatformHandle(url = '') {
+  let lastPart = url.split('/').pop()
+  if (lastPart) {
+    return lastPart
+  }
+  return url
+}
+
+const SOCIALS = [
+  {
+    platform: 'github',
+    handle: getPlatformHandle(siteMetadata.github),
+    href: siteMetadata.github,
+    Icon: () => <Github size={20} strokeWidth={1.5} />,
+    umamiEvent: 'profile-card-github',
+  },
+  {
+    platform: 'linkedin',
+    handle: getPlatformHandle(siteMetadata.linkedin),
+    href: siteMetadata.linkedin,
+    Icon: () => <Linkedin size={20} strokeWidth={1.5} />,
+    umamiEvent: 'profile-card-linkedin',
+  },
+  {
+    platform: 'x',
+    handle: getPlatformHandle(siteMetadata.x),
+    href: siteMetadata.x,
+    Icon: () => <XIcon className="h-4 w-4" fill="#fff" viewBox="0 0 1200 1227" />,
+    umamiEvent: 'profile-card-x',
+  },
+]
 
 export function ProfileCardInfo() {
   return (
@@ -10,7 +43,7 @@ export function ProfileCardInfo() {
       <h5 className="py-2 text-gray-700 dark:text-gray-400">Learner | Builder</h5>
       <div className="mb-2 mt-4 space-y-4">
         <div className="flex items-center text-gray-700 dark:text-gray-200">
-          <Backpack strokeWidth={1} size={20} />
+          <Backpack strokeWidth={1.5} size={20} />
           <p className="flex items-center space-x-1 px-2">
             <span>CTO & Co-Founder</span>
             <span>@</span>
@@ -25,7 +58,7 @@ export function ProfileCardInfo() {
           </p>
         </div>
         <div className="flex items-center text-gray-700 dark:text-gray-200">
-          <MapPin strokeWidth={1} size={20} />
+          <MapPin strokeWidth={1.5} size={20} />
           <p className="px-2">
             [::1]:443 - Ha Noi,
             <span className="absolute ml-1 inline-flex pt-px">
@@ -34,47 +67,30 @@ export function ProfileCardInfo() {
           </p>
         </div>
         <div className="flex items-center text-gray-700 dark:text-gray-200">
-          <Mail strokeWidth={1} size={20} />
+          <Mail strokeWidth={1.5} size={20} />
           <a className="px-2" href={`mailto:${siteMetadata.email}`}>
             {siteMetadata.email}
           </a>
         </div>
         <div className="flex items-center gap-2.5 text-gray-700 dark:text-gray-200">
-          <a
-            target="_blank"
-            href={siteMetadata.github}
-            rel="noreferrer"
-            className="flex items-center text-sm hover:underline"
-            data-umami-event="profile-card-github"
-          >
-            <Github size={20} strokeWidth={1} />
-            <span className="ml-px text-gray-500">/</span>
-            <span className="ml-0.5">{siteMetadata.github}</span>
-          </a>
-          <span className="text-gray-400 dark:text-gray-500">|</span>
-          <a
-            target="_blank"
-            href={siteMetadata.linkedin}
-            rel="noreferrer"
-            className="flex items-center text-sm hover:underline"
-            data-umami-event="profile-card-linkedin"
-          >
-            <Linkedin size={20} strokeWidth={1} />
-            <span className="ml-px text-gray-500">/</span>
-            <span className="ml-0.5">{siteMetadata.linkedin}</span>
-          </a>
-          <span className="text-gray-400 dark:text-gray-500">|</span>
-          <a
-            target="_blank"
-            href={siteMetadata.x}
-            rel="noreferrer"
-            className="flex items-center text-sm hover:underline"
-            data-umami-event="profile-card-x"
-          >
-            {/* <XIcon className="h-4 w-4" fill="#fff" viewBox="0 0 1200 1227" /> */}
-            <span className="ml-px text-gray-500">/</span>
-            <span className="ml-0.5">{siteMetadata.x}</span>
-          </a>
+          {SOCIALS.map(({ platform, handle, href, Icon, umamiEvent }, idx) => (
+            <Fragment key={platform}>
+              <a
+                target="_blank"
+                href={href}
+                rel="noreferrer"
+                className="flex items-center text-sm underline-offset-4 hover:underline"
+                data-umami-event={umamiEvent}
+              >
+                <Icon />
+                <span className="ml-px text-gray-500">/</span>
+                <span className="ml-0.5">{handle}</span>
+              </a>
+              {idx !== SOCIALS.length - 1 && (
+                <span className="text-gray-400 dark:text-gray-500">|</span>
+              )}
+            </Fragment>
+          ))}
         </div>
       </div>
     </div>
