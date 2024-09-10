b87cbb080fcc8435eaed4ff75d94e773458ae824
hta218 <huynhtuananh21895@gmail.com>
2024-09-10 10:15:14 +0700

    feat: handling query spotify now playing song

--- app/api/spotify/route.ts
+++ app/api/spotify/route.ts
@@ -1,9 +1,28 @@
-import { NewsletterAPI } from 'pliny/newsletter'
-import siteMetadata from '@/data/siteMetadata'
+import { getNowPlaying } from '~/libs/spotify.server'
+import type { SpotifyNowPlayingData } from '~/types/data'
 
-const handler = NewsletterAPI({
-  // @ts-ignore
-  provider: siteMetadata.newsletter.provider,
-})
+export async function GET() {
+  let response = await getNowPlaying()
+  if (response.status === 204 || response.status > 400) {
+    return Response.json({ isPlaying: false }, { status: response.status })
+  }
 
-export { handler as GET, handler as POST }
+  let data = await response.json()
+  if (data?.currently_playing_type === 'episode') {
+    return Response.json({
+      isPlaying: true,
+      title: data.item.name,
+      songUrl: data.item.external_urls.spotify,
+    })
+  }
+  let songData: SpotifyNowPlayingData = {
+    isPlaying: data.is_playing,
+    title: data.item.name,
+    artist: data.item.artists.map((art: { name: string }) => art.name).join(', '),
+    album: data.item.album.name,
+    albumImageUrl: data.item.album.images[0]?.url,
+    songUrl: data.item.external_urls.spotify,
+  }
+
+  return Response.json(songData)
+}
