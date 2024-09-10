ab9f88d47fbbcbb8bbd362e2215d3d72db136d71
hta218 <huynhtuananh21895@gmail.com>
2024-09-10 21:38:34 +0700

    refactor: update import paths in BlogLinks component

--- components/homepage/BlogLinks.tsx
+++ components/homepage/BlogLinks.tsx
@@ -1,46 +1,61 @@
 import siteMetadata from '@/data/siteMetadata'
-import Link from '../Link'
-import Twemoji from '../Twemoji'
+import Link from '~/components/Link'
+import Twemoji from '~/components/Twemoji'
+
+const LINKS = [
+  {
+    title: `What have I built?`,
+    href: `/projects`,
+    emoji: 'hammer-and-wrench',
+    event: 'home-link-projects',
+  },
+  {
+    title: `My writings`,
+    href: `/blog`,
+    emoji: 'memo',
+    event: 'home-link-blog',
+  },
+  {
+    title: `My snippets collection`,
+    href: `/snippets`,
+    emoji: 'dna',
+    event: 'home-link-snippets',
+  },
+  {
+    title: `More about me & myself`,
+    href: `/about`,
+    emoji: 'face-with-monocle',
+    event: 'home-link-about',
+  },
+  {
+    title: `My career`,
+    href: `/resume`,
+    emoji: 'briefcase',
+    event: 'home-link-resume',
+  },
+  {
+    title: `Traffic & engagement of this site`,
+    href: siteMetadata.analyticsUrl,
+    emoji: 'bar-chart',
+    event: 'home-link-analytics',
+  },
+]
 
 export function BlogLinks() {
   return (
-    <div className="flex flex-col space-y-1.5">
-      <Link href="/projects" className="underline-offset-4 hover:underline">
-        <Twemoji emoji="hammer-and-wrench" />
-        <span data-umami-event="home-link-projects" className="ml-1.5">
-          What have I built?
-        </span>
-      </Link>
-      <Link href="/blog" className="underline-offset-4 hover:underline">
-        <Twemoji emoji="memo" />
-        <span data-umami-event="home-link-blog" className="ml-1.5">
-          My writings
-        </span>
-      </Link>
-      <Link href="/snippets" className="underline-offset-4 hover:underline">
-        <Twemoji emoji="dna" />
-        <span data-umami-event="home-link-snippets" className="ml-1.5">
-          My snippets collection
-        </span>
-      </Link>
-      <Link href="/about" className="underline-offset-4 hover:underline">
-        <Twemoji emoji="face-with-monocle" />
-        <span data-umami-event="home-link-about" className="ml-1.5">
-          More about me & myself
-        </span>
-      </Link>
-      <Link href="/resume" className="underline-offset-4 hover:underline">
-        <Twemoji emoji="briefcase" />
-        <span data-umami-event="home-link-resume" className="ml-1.5">
-          My career
-        </span>
-      </Link>
-      <Link href={siteMetadata.analyticsUrl} className="underline-offset-4 hover:underline">
-        <Twemoji emoji="bar-chart" />
-        <span data-umami-event="home-link-analytics" className="ml-1.5">
-          Traffic & engagement of this site
-        </span>
-      </Link>
+    <div className="flex flex-col gap-3">
+      {LINKS.map(({ title, href, emoji, event }) => (
+        <Link
+          key={title}
+          href={href}
+          className="flex items-center underline-offset-4 hover:underline"
+        >
+          <Twemoji emoji={emoji} />
+          <span data-umami-event={event} className="ml-1.5 leading-6">
+            {title}
+          </span>
+        </Link>
+      ))}
     </div>
   )
 }
