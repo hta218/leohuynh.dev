ef968556c26b5c34bfea6a32cea72c753b4c5983
hta218 <huynhtuananh21895@gmail.com>
2024-09-11 19:59:08 +0700

    chore: update post layouts

--- layouts/PostBanner.tsx
+++ layouts/PostBanner.tsx
@@ -6,9 +6,11 @@ import PageTitle from '@/components/PageTitle'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 import siteMetadata from '@/data/siteMetadata'
 import type { Blog } from 'contentlayer/generated'
-import Bleed from 'pliny/ui/Bleed'
 import type { CoreContent } from 'pliny/utils/contentlayer'
 import type { ReactNode } from 'react'
+import { BlogMeta } from '~/components/blog/BlogMeta'
+import { BlogTags } from '~/components/blog/BlogTags'
+import { BannerInfo } from '~/components/BannerInfo'
 
 interface LayoutProps {
   content: CoreContent<Blog>
@@ -18,7 +20,7 @@ interface LayoutProps {
 }
 
 export default function PostMinimal({ content, next, prev, children }: LayoutProps) {
-  const { slug, title, images } = content
+  const { slug, title, images, bannerAuthor, bannerUrl, date, readingTime, tags } = content
   const displayImage =
     images && images.length > 0 ? images[0] : 'https://picsum.photos/seed/picsum/800/400'
 
@@ -26,52 +28,62 @@ export default function PostMinimal({ content, next, prev, children }: LayoutPro
     <Container>
       <ScrollTopAndComment />
       <article>
-        <div>
-          <div className="space-y-1 pb-10 text-center dark:border-gray-700">
-            <div className="w-full">
-              <Bleed>
-                <div className="relative aspect-[2/1] w-full">
-                  <Image src={displayImage} alt={title} fill className="object-cover" />
-                </div>
-              </Bleed>
+        <div className="space-y-6 py-6 xl:pb-16 xl:pt-16">
+          <BlogTags tags={tags} />
+          <PageTitle>{title}</PageTitle>
+          <dl>
+            <div>
+              <dt className="sr-only">Published on</dt>
+              <BlogMeta date={date} slug={slug} readingTime={readingTime} />
             </div>
-            <div className="relative pt-10">
-              <PageTitle>{title}</PageTitle>
-            </div>
-          </div>
-          <div className="prose max-w-none py-4 dark:prose-invert">{children}</div>
-          {siteMetadata.comments && (
-            <div className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300" id="comment">
-              <Comments slug={slug} />
-            </div>
-          )}
-          <footer>
-            <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
-              {prev && prev.path && (
-                <div className="pt-4 xl:pt-8">
-                  <Link
-                    href={`/${prev.path}`}
-                    className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    aria-label={`Previous post: ${prev.title}`}
-                  >
-                    &larr; {prev.title}
-                  </Link>
-                </div>
-              )}
-              {next && next.path && (
-                <div className="pt-4 xl:pt-8">
-                  <Link
-                    href={`/${next.path}`}
-                    className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    aria-label={`Next post: ${next.title}`}
-                  >
-                    {next.title} &rarr;
-                  </Link>
-                </div>
-              )}
-            </div>
-          </footer>
+          </dl>
         </div>
+        <div className="flex w-full flex-col gap-4">
+          <div className="-mx-5 md:-mx-10 lg:-mx-32 xl:-mx-40">
+            <Image
+              src={displayImage}
+              alt={title}
+              width={2000}
+              height={1000}
+              className="h-auto w-full rounded-xl object-cover"
+            />
+          </div>
+          <BannerInfo author={bannerAuthor} photoURL={bannerUrl} />
+        </div>
+        <div className="prose prose-lg max-w-none py-10 text-gray-900 dark:prose-invert">
+          {children}
+        </div>
+        {siteMetadata.comments && (
+          <div className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300" id="comment">
+            <Comments slug={slug} />
+          </div>
+        )}
+        <footer>
+          <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
+            {prev && prev.path && (
+              <div className="pt-4 xl:pt-8">
+                <Link
+                  href={`/${prev.path}`}
+                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                  aria-label={`Previous post: ${prev.title}`}
+                >
+                  &larr; {prev.title}
+                </Link>
+              </div>
+            )}
+            {next && next.path && (
+              <div className="pt-4 xl:pt-8">
+                <Link
+                  href={`/${next.path}`}
+                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                  aria-label={`Next post: ${next.title}`}
+                >
+                  {next.title} &rarr;
+                </Link>
+              </div>
+            )}
+          </div>
+        </footer>
       </article>
     </Container>
   )
--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -94,7 +94,9 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               </dd>
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+              <div className="prose prose-lg max-w-none pb-8 pt-10 dark:prose-invert">
+                {children}
+              </div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={discussUrl(path)} rel="nofollow">
                   Discuss on Twitter
