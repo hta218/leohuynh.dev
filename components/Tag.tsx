32608a1945886f2ed43346f2718f6a1affbdb749
hta218 <huynhtuananh21895@gmail.com>
2024-09-12 09:13:57 +0700

    style: update tags

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -5,7 +5,7 @@ function Tag({ text }: { text: string }) {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="mr-3 font-medium text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      className="mr-3 rounded-lg bg-slate-100 px-2 py-0.5 text-sm font-semibold text-gray-600 hover:text-gray-800"
     >
       #{text.split(' ').join('-')}
     </Link>
