972e893bac285bbee813a56faea5d09b345330b7
hta218 <huynhtuananh21895@gmail.com>
2024-09-12 23:48:09 +0700

    chore: update `CommentsComponent` to load comment instantly

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,22 +1,11 @@
 'use client'
 
 import { Comments as CommentsComponent } from 'pliny/comments'
-import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
-
   if (!siteMetadata.comments?.provider) {
     return null
   }
-  return (
-    <>
-      {loadComments ? (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      ) : (
-        <button onClick={() => setLoadComments(true)}>Load Comments</button>
-      )}
-    </>
-  )
+  return <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
 }
