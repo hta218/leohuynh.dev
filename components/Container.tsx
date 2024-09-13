1af4340eaac5857e4b507c27101b15c01707fca9
hta218 <huynhtuananh21895@gmail.com>
2024-09-13 23:05:49 +0700

    refactor: update Container component layout to make it full width

--- components/Container.tsx
+++ components/Container.tsx
@@ -9,7 +9,9 @@ interface Props {
 
 export default function Container({ children, as: Component = 'section', className }: Props) {
   return (
-    <Component className={clsx('mx-auto max-w-3xl px-3 sm:px-6 xl:max-w-5xl xl:px-0', className)}>
+    <Component
+      className={clsx('mx-auto w-full max-w-3xl px-3 sm:px-6 xl:max-w-5xl xl:px-0', className)}
+    >
       {children}
     </Component>
   )
