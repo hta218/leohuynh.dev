3f8e340c09594ab34a602cadd3b1ec98834b20a2
hta218 <huynhtuananh21895@gmail.com>
2024-09-14 10:21:53 +0700

    refactor: update tag-data, remove resume page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,6 @@ const HEADER_NAV_LINKS = [
   { href: '/snippets', title: 'Snippets' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: '/resume', title: 'Resume' },
 ]
 
 export default HEADER_NAV_LINKS
