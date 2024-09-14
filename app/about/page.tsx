93c227d84421c9049839371d38fcde7be63d60ed
hta218 <huynhtuananh21895@gmail.com>
2024-09-14 10:18:51 +0700

    feat: add static resume pdf file

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,17 +5,15 @@ import { AuthorLayout } from '~/layouts/author-layout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'About' })
+export let metadata = genPageMetadata({ title: 'About' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
+  let author = allAuthors.find((p) => p.slug === 'default') as Authors
+  let mainContent = coreContent(author)
 
   return (
-    <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
-    </>
+    <AuthorLayout content={mainContent}>
+      <MDXLayoutRenderer code={author.body.code} />
+    </AuthorLayout>
   )
 }
