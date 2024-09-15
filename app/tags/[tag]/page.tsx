bc0ac5a225dd53eae99c774827dc0c667aa33bbc
hta218 <huynhtuananh21895@gmail.com>
2024-09-15 15:28:04 +0700

    refactor: update tag page and list layout components

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -9,7 +9,7 @@ import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 export async function generateMetadata({ params }: { params: { tag: string } }): Promise<Metadata> {
-  const tag = decodeURI(params.tag)
+  let tag = decodeURI(params.tag)
   return genPageMetadata({
     title: tag,
     description: `${siteMetadata.title} ${tag} tagged content`,
@@ -22,20 +22,20 @@ export async function generateMetadata({ params }: { params: { tag: string } }):
   })
 }
 
-export const generateStaticParams = async () => {
-  const tagCounts = tagData as Record<string, number>
-  const tagKeys = Object.keys(tagCounts)
-  const paths = tagKeys.map((tag) => ({
+export let generateStaticParams = async () => {
+  let tagCounts = tagData as Record<string, number>
+  let tagKeys = Object.keys(tagCounts)
+  let paths = tagKeys.map((tag) => ({
     tag: encodeURI(tag),
   }))
   return paths
 }
 
 export default function TagPage({ params }: { params: { tag: string } }) {
-  const tag = decodeURI(params.tag)
+  let tag = decodeURI(params.tag)
   // Capitalize first letter and convert space to dash
-  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
-  const filteredPosts = allCoreContent(
+  let title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
+  let filteredPosts = allCoreContent(
     sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
   )
   if (filteredPosts.length === 0) {
--- layouts/list-layout-with-tags.tsx
+++ layouts/list-layout-with-tags.tsx
@@ -1,79 +1,25 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 'use client'
 
-import { usePathname } from 'next/navigation'
-import { slug } from 'github-slugger'
-import { formatDate } from 'pliny/utils/formatDate'
-import type { CoreContent } from 'pliny/utils/contentlayer'
-import type { Blog } from 'contentlayer/generated'
 import Link from '@/components/Link'
-import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
 import tagData from 'app/tag-data.json'
+import type { Blog } from 'contentlayer/generated'
+import { slug } from 'github-slugger'
+import { usePathname } from 'next/navigation'
+import type { CoreContent } from 'pliny/utils/contentlayer'
 import Container from '~/components/Container'
+import { PostCardGridView } from '~/components/blog/post-card-grid-view'
 
-interface PaginationProps {
-  totalPages: number
-  currentPage: number
-}
 interface ListLayoutProps {
   posts: CoreContent<Blog>[]
   title: string
-  initialDisplayPosts?: CoreContent<Blog>[]
-  pagination?: PaginationProps
 }
 
-function Pagination({ totalPages, currentPage }: PaginationProps) {
-  let pathname = usePathname()
-  let basePath = pathname.split('/')[1]
-  let prevPage = currentPage - 1 > 0
-  let nextPage = currentPage + 1 <= totalPages
-
-  return (
-    <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-      <nav className="flex justify-between">
-        {!prevPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
-            Previous
-          </button>
-        )}
-        {prevPage && (
-          <Link
-            href={currentPage - 1 === 1 ? `/${basePath}/` : `/${basePath}/page/${currentPage - 1}`}
-            rel="prev"
-          >
-            Previous
-          </Link>
-        )}
-        <span>
-          {currentPage} of {totalPages}
-        </span>
-        {!nextPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
-            Next
-          </button>
-        )}
-        {nextPage && (
-          <Link href={`/${basePath}/page/${currentPage + 1}`} rel="next">
-            Next
-          </Link>
-        )}
-      </nav>
-    </div>
-  )
-}
-
-export function ListLayoutWithTags({
-  posts,
-  title,
-  initialDisplayPosts = [],
-  pagination,
-}: ListLayoutProps) {
+export function ListLayoutWithTags({ posts, title }: ListLayoutProps) {
   let pathname = usePathname()
   let tagCounts = tagData as Record<string, number>
   let tagKeys = Object.keys(tagCounts)
   let sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
-  let displayPosts = initialDisplayPosts.length > 0 ? initialDisplayPosts : posts
 
   return (
     <Container>
@@ -119,43 +65,13 @@ export function ListLayoutWithTags({
           </div>
         </div>
         <div>
-          <ul>
-            {displayPosts.map((post) => {
-              let { path, date, title, summary, tags } = post
-              return (
-                <li key={path} className="py-5">
-                  <article className="flex flex-col space-y-2 xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date} suppressHydrationWarning>
-                          {formatDate(date, siteMetadata.locale)}
-                        </time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-3">
-                      <div>
-                        <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                          <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
-                            {title}
-                          </Link>
-                        </h2>
-                        <div className="flex flex-wrap">
-                          {tags?.map((tag) => <Tag key={tag} text={tag} />)}
-                        </div>
-                      </div>
-                      <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                        {summary}
-                      </div>
-                    </div>
-                  </article>
-                </li>
-              )
-            })}
+          <ul className="grid grid-cols-1 gap-x-8 gap-y-12 lg:grid-cols-2">
+            {posts.map((post) => (
+              <li key={post.path}>
+                <PostCardGridView post={post} />
+              </li>
+            ))}
           </ul>
-          {pagination && pagination.totalPages > 1 && (
-            <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
-          )}
         </div>
       </div>
     </Container>
