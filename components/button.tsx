2afd041d0fe94b05a871ac16adfb585e58e2fbde
hta218 <huynhtuananh21895@gmail.com>
2024-09-15 10:18:02 +0700

    refactor: update `Button` to accept more props

--- components/button.tsx
+++ components/button.tsx
@@ -4,24 +4,30 @@ export function Button({
   children,
   as: Component = 'button',
   className,
+  ...rest
 }: {
   children: React.ReactNode
   as?: React.ElementType
   className?: string
+  [key: string]: any
 }) {
   return (
     <Component
       className={clsx([
         'border border-transparent',
-        'bg-blue-600 text-white hover:bg-blue-700 dark:hover:bg-blue-500',
+        'bg-blue-600 text-white hover:bg-blue-700 hover:text-white dark:hover:bg-blue-500',
         'focus:shadow-outline-blue focus:outline-none',
         'transition-colors duration-150',
         'text-sm font-medium leading-5',
         'inline rounded-lg px-4 py-2 shadow',
+        'inline-flex items-center gap-1 no-underline',
         className,
       ])}
+      {...rest}
     >
       {children}
     </Component>
   )
 }
+
+export default Button
