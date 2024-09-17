8959b1e654518849449bb851bbce3f937025aaba
hta218 <huynhtuananh21895@gmail.com>
2024-09-17 15:20:32 +0700

    feat: query project github repo data server side

---
+++ types/server.ts
@@ -0,0 +1,20 @@
+export interface ViewApiResponse {
+  data?: {
+    total: string
+  }
+}
+
+export type GithubRepository = {
+  stargazerCount: number
+  description: string
+  homepageUrl: string
+  languages: {
+    color: string
+    name: string
+  }[]
+  name: string
+  nameWithOwner: string
+  url: string
+  forkCount: number
+  repositoryTopics: string[]
+}
