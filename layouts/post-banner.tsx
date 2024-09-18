470291f00448ecfe5f6997cac02ff22b0aa3e46d
hta218 <huynhtuananh21895@gmail.com>
2024-09-18 23:04:13 +0700

    refactor: update post layouts

--- layouts/post-banner.tsx
+++ layouts/post-banner.tsx
@@ -8,7 +8,6 @@ import Image from '~/components/Image'
 import PageTitle from '~/components/PageTitle'
 import ScrollTopAndComment from '~/components/ScrollTopAndComment'
 import { SocialShareButtons } from '~/components/SocialShareButtons'
-import { BackToPosts } from '~/components/blog/back-to-posts'
 import { BlogMeta } from '~/components/blog/blog-meta'
 import { BlogTags } from '~/components/blog/blog-tags'
 import siteMetadata from '~/data/siteMetadata'
@@ -20,7 +19,7 @@ interface LayoutProps {
   prev?: { path: string; title: string }
 }
 
-export function PostBanner({ content, next, prev, children }: LayoutProps) {
+export function PostBanner({ content, children }: LayoutProps) {
   let {
     slug,
     type,
@@ -38,7 +37,7 @@ export function PostBanner({ content, next, prev, children }: LayoutProps) {
   let postUrl = `${siteMetadata.siteUrl}/${type.toLowerCase()}/${slug}`
 
   return (
-    <Container className="sm:pt-4 lg:pt-10">
+    <Container className="pt-4 lg:pt-12">
       <ScrollTopAndComment />
       <article className="space-y-6 pt-6 lg:space-y-16">
         <div className="space-y-4">
@@ -64,38 +63,12 @@ export function PostBanner({ content, next, prev, children }: LayoutProps) {
           </div>
         </div>
         <div className="prose prose-lg max-w-none dark:prose-invert">{children}</div>
-        <div className="border-t border-gray-200 pt-4 dark:border-gray-700 md:pb-10 md:pt-10">
+        <div className="border-t border-gray-200 dark:border-gray-700 md:pb-10 md:pt-4">
+          <SocialShareButtons postUrl={postUrl} title={title} filePath={filePath} />
           <div id="comment">
             <Comments slug={slug} />
           </div>
-          <SocialShareButtons postUrl={postUrl} title={title} filePath={filePath} />
         </div>
-        {/* <footer>
-          <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
-            {prev && prev.path && (
-              <div className="pt-4 xl:pt-8">
-                <Link
-                  href={`/${prev.path}`}
-                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                  aria-label={`Previous post: ${prev.title}`}
-                >
-                  &larr; {prev.title}
-                </Link>
-              </div>
-            )}
-            {next && next.path && (
-              <div className="pt-4 xl:pt-8">
-                <Link
-                  href={`/${next.path}`}
-                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                  aria-label={`Next post: ${next.title}`}
-                >
-                  {next.title} &rarr;
-                </Link>
-              </div>
-            )}
-          </div>
-        </footer> */}
       </article>
     </Container>
   )
