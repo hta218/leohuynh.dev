e547b1fc05b3677fe3a2be0b1bd51553f44e8353
hta218 <huynhtuananh21895@gmail.com>
2024-09-18 14:29:55 +0700

    style: update logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,17 +19,21 @@ function Header() {
     <header
       className={clsx(
         'bg-white/75 py-3 backdrop-blur dark:bg-dark/75',
-        siteMetadata.stickyNav && 'sticky top-0 z-50'
+        siteMetadata.stickyNav && 'sticky top-2 z-50 lg:top-4'
       )}
     >
       <Container as="div" className="flex items-center justify-between gap-3">
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
+        <Link
+          href="/"
+          aria-label={siteMetadata.headerTitle}
+          className="rounded-xl p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:ring-white/10"
+        >
           <NextImage
             src="/static/images/logo.jpg"
             alt={siteMetadata.headerTitle}
-            width={45}
-            height={45}
-            className="rounded-xl"
+            width={100}
+            height={100}
+            className="h-10 w-10 rounded-xl"
             priority
           />
         </Link>
