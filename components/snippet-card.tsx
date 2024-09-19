37c7ca23c236b1289648f1cfc0f416d35470d7aa
hta218 <huynhtuananh21895@gmail.com>
2024-09-19 21:38:42 +0700

    feat: add `SnippetCard`

---
+++ components/snippet-card.tsx
@@ -0,0 +1,44 @@
+import { clsx } from 'clsx'
+import { type Snippet } from 'contentlayer/generated'
+import { type CoreContent } from 'pliny/utils/contentlayer'
+import type { BrandsMap } from '~/components/Brands'
+import { Brand } from '~/components/Brands'
+import { GradientBorder } from '~/components/gradient-border'
+import { GrowingUnderline } from '~/components/growing-underline'
+import Link from '~/components/Link'
+import { TiltedGridBackground } from '~/components/ui/tilted-grid-background'
+
+export function SnippetCard({ snippet }: { snippet: CoreContent<Snippet> }) {
+  let { icon, heading, summary, title, path } = snippet
+  return (
+    <GradientBorder className="rounded-2xl">
+      <Link
+        href={`/${path}`}
+        title={title}
+        className={clsx([
+          'relative flex h-full rounded-2xl',
+          'bg-zinc-50 dark:bg-white/5',
+          'transition-shadow hover:shadow-md',
+          'hover:shadow-zinc-900/5 dark:hover:shadow-black/15',
+        ])}
+      >
+        <TiltedGridBackground className="inset-0" />
+        <Brand
+          type={icon as keyof typeof BrandsMap}
+          as="icon"
+          className="absolute -top-5 left-4 z-10 h-12 w-12 text-gray-900 dark:text-white"
+        />
+        <div className="relative w-full px-4 pb-6 pt-6">
+          <h3 className="mt-4 text-xl font-semibold leading-7">
+            <GrowingUnderline>{heading}</GrowingUnderline>
+          </h3>
+          <p className="mt-1.5 line-clamp-2 text-zinc-600 dark:text-zinc-400">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum corrupti dolorem
+            aspernatur voluptas, adipisci pariatur magnam corporis blanditiis velit dicta neque qui
+            minus ab at dolor vitae eligendi quaerat nihil.
+          </p>
+        </div>
+      </Link>
+    </GradientBorder>
+  )
+}
