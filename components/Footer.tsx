b048ca5240fb74a1331316856fadfe1e50f90064
hta218 <huynhtuananh21895@gmail.com>
2024-09-19 21:37:55 +0700

    refactor: update footer layout component and structure

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,20 +7,19 @@ import Link from './Link'
 
 export function Footer() {
   return (
-    <Container
-      as="footer"
-      className="mt-8 items-center justify-between space-y-4 border-t border-gray-200 py-8 dark:border-gray-700 lg:flex"
-    >
-      <BuiltWith />
-      <div className="my-2 flex flex-wrap items-center gap-1.5 text-gray-500 dark:text-gray-400">
-        <span className="hidden md:block">{`© ${new Date().getFullYear()}`}</span>
-        <span className="hidden md:block">-</span>
-        <span>{siteMetadata.title}</span>
-        <Link href={siteMetadata.siteRepo}>
-          <span data-umami-event="made-in-vietnam">
-            <MadeInVietNameIcon />
-          </span>
-        </Link>
+    <Container as="footer" className="mt-8">
+      <div className="items-center justify-between space-y-4 border-t border-gray-200 py-8 dark:border-gray-700 lg:flex">
+        <BuiltWith />
+        <div className="my-2 flex flex-wrap items-center gap-1.5 text-gray-500 dark:text-gray-400">
+          <span className="hidden md:block">{`© ${new Date().getFullYear()}`}</span>
+          <span className="hidden md:block">-</span>
+          <span>{siteMetadata.title}</span>
+          <Link href={siteMetadata.siteRepo}>
+            <span data-umami-event="made-in-vietnam">
+              <MadeInVietNameIcon />
+            </span>
+          </Link>
+        </div>
       </div>
     </Container>
   )
