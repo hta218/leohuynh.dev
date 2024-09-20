ab83e6431cf36a4e070b17854eb1f935bb29963b
hta218 <huynhtuananh21895@gmail.com>
2024-09-20 10:56:03 +0700

    refactor: update NotFound component and related styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,17 @@
-import Image from 'next/image'
 import { Button } from '~/components/button'
 import Container from '~/components/Container'
 import Link from '~/components/Link'
 import Twemoji from '~/components/Twemoji'
+import FourOhFour from '~/icons/404.svg'
 
 export default function NotFound() {
   return (
     <Container className="pt-4 lg:pt-12">
       <div className="flex flex-col items-center justify-center py-6">
-        <Image
-          src="/static/images/404.png"
-          alt="404"
-          width={500}
-          height={500}
-          className="max-w-[80vw]"
-        />
+        <FourOhFour className="mx-auto aspect-square w-[300px] max-w-[80vw] md:w-[500px]" />
         <div className="space-x-2 pt-8 md:space-y-5 md:pt-12 xl:pt-16">
           <div className="max-w-md text-center">
-            <p className="mb-4 text-2xl font-bold leading-normal md:text-2xl">
+            <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
               Hmm... it looks like you're lost.
               <Twemoji emoji={'face-with-monocle'} />
             </p>
