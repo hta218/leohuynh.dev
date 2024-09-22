53517c521577633bf9762a757c2d98ecf9fc6c1c
hta218 <huynhtuananh21895@gmail.com>
2024-09-22 08:47:39 +0700

    refactor: update next.config.js to include additional image hostnames

--- next.config.js
+++ next.config.js
@@ -77,6 +77,10 @@ module.exports = () => {
           protocol: 'https',
           hostname: 'picsum.photos',
         },
+        {
+          protocol: 'https',
+          hostname: 'i.gr-assets.com',
+        },
       ],
       unoptimized,
     },
