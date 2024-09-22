ec9227a34a7fc4018119afafc22b65b2c0d6c587
hta218 <huynhtuananh21895@gmail.com>
2024-09-22 13:57:38 +0700

    refactor: update Tag component to accept size prop

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,12 +1,18 @@
 import Link from 'next/link'
 import { slug } from 'github-slugger'
+import { clsx } from 'clsx'
 
-function Tag({ text }: { text: string }) {
+function Tag({ text, size = 'sm' }: { text: string; size?: 'sm' | 'md' }) {
   let tagName = text.split(' ').join('-')
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="rounded-lg bg-slate-100 px-2 py-0.5 text-sm font-semibold text-gray-600 hover:text-gray-800 dark:bg-gray-700 dark:text-gray-300"
+      className={clsx([
+        'rounded-lg px-2 py-0.5 font-semibold',
+        'bg-slate-100 text-gray-600 hover:text-gray-800',
+        'dark:bg-gray-700 dark:text-gray-300',
+        size === 'sm' ? 'text-sm' : 'text-base',
+      ])}
     >
       <span data-umami-event={`tag-${tagName}`}>#{tagName}</span>
     </Link>
