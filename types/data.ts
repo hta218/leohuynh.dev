7358fb74a53fb4d32f842d4001682baca8232ade
hta218 <huynhtuananh21895@gmail.com>
2024-09-22 08:50:51 +0700

    refactor: remove unused import in types/data.ts and add GoodreadsBook type

--- types/data.ts
+++ types/data.ts
@@ -1,5 +1,3 @@
-import type { GithubRepository } from './server'
-
 export type SpotifyNowPlayingData = {
   isPlaying: boolean
   songUrl?: string
@@ -17,4 +15,50 @@ export type Project = {
   url?: string
   repo?: string | GithubRepository | null
   builtWith: string[]
+}
+
+export type GoodreadsBook = {
+  guid: string
+  pubDate: string
+  title: string
+  link: string
+  book_id: string
+  book_image_url: string
+  book_small_image_url: string
+  book_medium_image_url: string
+  book_large_image_url: string
+  book_description: string
+  author_name: string
+  isbn: string
+  user_name: string
+  user_rating: number
+  user_read_at: string
+  user_date_added: string
+  user_date_created: string
+  user_shelves: string
+  user_review: string
+  average_rating: number
+  book_published: string
+  content: string
+}
+
+export interface ViewApiResponse {
+  data?: {
+    total: string
+  }
+}
+
+export type GithubRepository = {
+  stargazerCount: number
+  description: string
+  homepageUrl: string
+  languages: {
+    color: string
+    name: string
+  }[]
+  name: string
+  nameWithOwner: string
+  url: string
+  forkCount: number
+  repositoryTopics: string[]
 }
