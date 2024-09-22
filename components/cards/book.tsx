0f827269eddc6904cbadaad2fbf59ddcc7b6ff1e
hta218 <huynhtuananh21895@gmail.com>
2024-09-22 13:56:24 +0700

    refactor: update `BookCard` UI and structure

--- components/cards/book.tsx
+++ components/cards/book.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import clsx from 'clsx'
 import { PenTool, Quote, Star } from 'lucide-react'
 import { useState } from 'react'
@@ -11,13 +13,10 @@ import { TiltedGridBackground } from '~/components/ui/tilted-grid-background'
 import type { GoodreadsBook } from '~/types/data'
 
 export function BookCard({ book }: { book: GoodreadsBook }) {
-  let url = getBookUrl(book.content)
-  let [tab, setTab] = useState<'summary' | 'review'>('summary')
-
   return (
-    <GradientBorder className="flex gap-8 rounded-xl p-6 dark:bg-white/5">
-      <TiltedGridBackground className="inset-0" />
-      <div className="flex w-60 shrink-0 items-center justify-center">
+    <GradientBorder className="flex flex-col gap-8 rounded-2xl px-3 py-6 dark:bg-white/5 md:flex-row md:px-6">
+      <TiltedGridBackground className="inset-0 z-[-1]" />
+      <div className="mx-auto flex w-60 shrink-0 items-center justify-center">
         <Image
           src={book.book_large_image_url}
           alt={book.title}
@@ -30,104 +29,132 @@ export function BookCard({ book }: { book: GoodreadsBook }) {
       <div className="flex grow flex-col justify-between gap-8">
         <div className="space-y-4">
           <div className="space-y-2">
-            <div className="flex items-start justify-between gap-8">
+            <div className="flex items-start justify-between gap-12 text-xl font-semibold md:text-2xl">
               {book.link ? (
-                <Link href={book.link} className="text-2xl font-semibold">
+                <Link href={book.link}>
                   <GrowingUnderline>{book.title}</GrowingUnderline>
                 </Link>
               ) : (
-                <h3 className="text-2xl font-semibold">{book.title}</h3>
+                <h3>{book.title}</h3>
               )}
-              <span className="inline-flex shrink-0 items-center gap-1 rounded-full bg-gray-200 px-3 py-0.5 dark:bg-gray-700">
-                <Star size={18} strokeWidth={1.5} />
-                <span>{book.user_rating}</span>
-              </span>
+              <UserRating rating={book.user_rating} className="hidden md:inline-flex" />
             </div>
           </div>
-          <div className="space-y-2">
-            <div className="-ml-1 flex items-center gap-2">
-              <button
-                onClick={() => setTab('summary')}
-                className={clsx(
-                  'inline-flex items-center gap-1 font-medium underline-offset-4',
-                  tab === 'summary'
-                    ? 'underline'
-                    : 'text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100'
-                )}
-              >
-                <Twemoji emoji="spiral-notepad" />
-                <span>Summary</span>
-              </button>
-              {book.user_review && (
-                <>
-                  <span>/</span>
-                  <button
-                    className={clsx(
-                      'inline-flex items-center gap-1 font-medium underline-offset-4',
-                      tab === 'review'
-                        ? 'underline'
-                        : 'text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100'
-                    )}
-                    onClick={() => setTab('review')}
-                  >
-                    <Twemoji emoji="glowing-star" />
-                    <span>My review</span>
-                  </button>
-                </>
-              )}
-            </div>
-            <div className="relative pr-4">
-              {tab === 'summary' ? (
-                <>
-                  <Quote
-                    size={20}
-                    strokeWidth={1.5}
-                    className="absolute -top-2 right-0 z-[-1] h-10 w-10 text-gray-200 dark:text-gray-700"
-                  />
-                  <p
-                    className="line-clamp-5 font-medium text-gray-600 dark:text-gray-400"
-                    dangerouslySetInnerHTML={{ __html: book.book_description }}
-                  />
-                </>
-              ) : (
-                <>
-                  <PenTool
-                    size={20}
-                    strokeWidth={1.5}
-                    className="absolute -top-2 right-0 z-[-1] h-10 w-10 text-gray-200 dark:text-gray-700"
-                  />
-                  <p className="dark:text-gray-450 line-clamp-3 font-medium text-gray-600 dark:text-gray-400">
-                    {book.user_review}
-                  </p>
-                </>
-              )}
-            </div>
-          </div>
+          <BookDetails book={book} />
         </div>
         <div className="flex items-center justify-between">
-          <div className="flex items-center gap-1 text-gray-600 dark:text-gray-400">
-            <div className="flex items-center gap-1">
-              <Twemoji emoji="writing-hand" /> by{' '}
-              <span className="font-semibold">{book.author_name}</span>
-            </div>
-            <div className="flex items-center gap-1">
-              <span className="font-medium">(avg. {book.average_rating}/5)</span>
-            </div>
-          </div>
-          {url && (
-            <Link href={url}>
-              <GrowingUnderline>
-                <Brand
-                  type="Goodreads"
-                  as="icon"
-                  className="h-5 text-goodreads dark:text-gray-100"
-                />
-              </GrowingUnderline>
-            </Link>
-          )}
+          <BookMeta book={book} />
+          <GoodreadsLink url={getBookUrl(book.content)} />
         </div>
       </div>
     </GradientBorder>
+  )
+}
+
+function BookMeta({ book }: { book: GoodreadsBook }) {
+  return (
+    <div className="flex items-center gap-1 text-gray-600 dark:text-gray-400">
+      <div className="flex items-center gap-1">
+        <Twemoji emoji="writing-hand" /> by{' '}
+        <span className="font-semibold">{book.author_name}</span>
+      </div>
+      <div className="hidden items-center gap-1 md:flex">
+        <span>(avg. {book.average_rating}/5)</span>
+      </div>
+    </div>
+  )
+}
+
+function BookDetails({ book }: { book: GoodreadsBook }) {
+  let [tab, setTab] = useState<'summary' | 'review'>('summary')
+  return (
+    <div className="space-y-3">
+      <div className="-ml-1 flex items-center gap-2">
+        <button
+          onClick={() => setTab('summary')}
+          className={clsx(
+            'inline-flex items-center gap-1 font-medium underline-offset-4',
+            tab === 'summary'
+              ? 'underline'
+              : 'text-gray-500 hover:text-gray-900 dark:hover:text-gray-100'
+          )}
+        >
+          <Twemoji emoji="spiral-notepad" />
+          <span>Summary</span>
+        </button>
+        {book.user_review && (
+          <>
+            <span>/</span>
+            <button
+              className={clsx(
+                'inline-flex items-center gap-1 font-medium underline-offset-4',
+                tab === 'review'
+                  ? 'underline'
+                  : 'text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100'
+              )}
+              onClick={() => setTab('review')}
+            >
+              <Twemoji emoji="glowing-star" />
+              <span>My review</span>
+              <span className="md:hidden">({book.user_rating}/5)</span>
+            </button>
+          </>
+        )}
+      </div>
+      <div className="relative md:pr-4">
+        {tab === 'summary' ? (
+          <>
+            <Quote
+              size={20}
+              strokeWidth={1.5}
+              className="absolute -top-4 right-0 z-[-1] h-10 w-10 text-gray-200 dark:text-gray-700 md:-top-2"
+            />
+            <p className="line-clamp-5 font-medium italic text-gray-600 dark:text-gray-400">
+              "{book.book_description}"
+            </p>
+          </>
+        ) : (
+          <>
+            <PenTool
+              size={20}
+              strokeWidth={1.5}
+              className="absolute -top-4 right-0 z-[-1] h-10 w-10 text-gray-200 dark:text-gray-700 md:-top-2"
+            />
+            <p className="font-medium text-gray-600 dark:text-gray-400">{book.user_review}</p>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
+
+function GoodreadsLink({ url, className }: { url?: string | null; className?: string }) {
+  if (url) {
+    return (
+      <Link href={url} className={className}>
+        <Brand type="Goodreads" as="icon" className="h-5 text-goodreads dark:text-gray-100" />
+      </Link>
+    )
+  }
+  return null
+}
+
+function UserRating({ rating, className }: { rating: number; className?: string }) {
+  return (
+    <span
+      className={clsx([
+        'text-base',
+        'shrink-0 items-center gap-1',
+        'rounded-full px-3 py-0.5',
+        'font-medium text-gray-700 dark:text-gray-900',
+        'bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-yellow-200 via-emerald-200 to-yellow-200',
+        'dark:bg-gradient-to-l dark:from-emerald-500 dark:to-lime-600',
+        className,
+      ])}
+    >
+      <Star size={18} strokeWidth={1.5} />
+      <span>{rating}</span>
+    </span>
   )
 }
 
