b4a3cb9cd996aee212a6e1ac165e47d3124ff550
hta218 <huynhtuananh21895@gmail.com>
2024-09-23 23:10:47 +0700

    refactor: remove unused site metadata and update analytics configuration

--- data/site-metadata.ts
+++ data/site-metadata.ts
@@ -10,7 +10,6 @@ export const SITE_METADATA = {
   siteRepo: 'https://github.com/hta218/leohuynh.dev',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.jpg`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.jpeg`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'leohuynh@pm.me',
   github: 'https://github.com/hta218',
   x: 'https://x.com/hta218_',
@@ -21,12 +20,11 @@ export const SITE_METADATA = {
   instagram: 'https://www.instagram.com/hta218_',
   locale: 'en-US',
   stickyNav: true,
+  goodreadsFeedUrl: 'https://www.goodreads.com/review/list_rss/179720035',
   analyticsUrl: 'https://analytics.leohuynh.dev/share/jkwRskv0/leohuynh.dev',
   analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
     umamiAnalytics: {
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID!, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      umamiWebsiteId: process.env.NEXT_UMAMI_ID!,
       // You may also need to overwrite the script if you're storing data in the US - ex:
       // src: 'https://us.umami.is/script.js'
       // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
@@ -38,13 +36,8 @@ export const SITE_METADATA = {
     provider: 'buttondown',
   },
   comments: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
     provider: 'giscus', // supported providers: giscus, utterances, disqus
     giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
       repo: process.env.NEXT_PUBLIC_GISCUS_REPO!,
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID!,
@@ -52,35 +45,18 @@ export const SITE_METADATA = {
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID!,
       mapping: 'title', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
       theme: 'light',
-      // theme when dark mode
       darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
       lang: 'en',
     },
   },
   search: {
-    provider: 'kbar', // kbar or algolia
+    provider: 'kbar',
     kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
+      // path to load documents to search
+      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`,
     },
-    // provider: 'algolia',
-    // algoliaConfig: {
-    //   // The application ID provided by Algolia
-    //   appId: 'R2IYF7ETH7',
-    //   // Public API key: it is safe to commit it
-    //   apiKey: '599cec31baffa4868cae4e79f180729b',
-    //   indexName: 'docsearch',
-    // },
   },
-  // Custom properties
-  goodreadsFeedUrl: 'https://www.goodreads.com/review/list_rss/179720035',
 }
