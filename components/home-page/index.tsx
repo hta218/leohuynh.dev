bcbe50974d98a106c79e7f9bc3a6ae784a5480c5
hta218 <huynhtuananh21895@gmail.com>
2024-09-23 22:48:37 +0700

    refactor: update home components

--- components/home-page/index.tsx
+++ components/home-page/index.tsx
@@ -1,14 +1,13 @@
 import type { CoreContent } from 'pliny/utils/contentlayer'
 import type { Blog } from '~/.contentlayer/generated'
 import { ProfileCard } from '~/components/cards/profile'
-import { BlogLinks } from '~/components/home-page/BlogLinks'
-import { Greeting } from '~/components/home-page/Greeting'
-import { Heading } from '~/components/home-page/Heading'
-import { LatestPosts } from '~/components/home-page/LatestPosts'
-import { ShortDescription } from '~/components/home-page/ShortDescription'
-import { TypedBios } from '~/components/home-page/TypedBios'
 import { Container } from '~/components/ui/container'
 import { Twemoji } from '~/components/ui/twemoji'
+import { Greeting } from './greeting2'
+import { Intro } from './intro'
+import { LatestPosts } from './latest-posts'
+import { BlogLinks } from './links'
+import { TypedBios } from './typed-bios'
 
 export function Home({ posts }: { posts: CoreContent<Blog>[] }) {
   return (
@@ -17,9 +16,14 @@ export function Home({ posts }: { posts: CoreContent<Blog>[] }) {
         <div className="space-y-4 md:space-y-6 md:pr-8 xl:col-span-2">
           <Greeting />
           <div className="text-base leading-7 text-gray-600 dark:text-gray-400 md:text-lg md:leading-8">
-            <Heading />
+            <Intro />
             <TypedBios />
-            <ShortDescription />
+            <div className="mb-6 mt-4 md:mb-8">
+              <p>I started learning to code in 2016 and have been hooked ever since.</p>
+              <p>I landed my first job as a Python coding mentor in 2017.</p>
+              <p>I have a passion for JS/TS, web dev, and eCommerce.</p>
+              <p>I started this blog to document and share my knowledge & experience.</p>
+            </div>
             <BlogLinks />
             <p className="my-6 flex md:my-8">
               <span className="mr-2">Happy reading</span>
--- components/home-page/intro.tsx
+++ components/home-page/intro.tsx
@@ -1,6 +1,6 @@
 import { Twemoji } from '~/components/ui/twemoji'
 
-export function Heading() {
+export function Intro() {
   return (
     <h1 className="text-neutral-900 dark:text-neutral-200">
       I'm <span className="font-medium">Tuan Anh Huynh</span> - a passionate Software Engineer in
