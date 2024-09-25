1871d51d86be2c66fa77d2fb3303c817f1763723
hta218 <huynhtuananh21895@gmail.com>
2024-09-25 21:44:52 +0700

    refactor: update header nav links with emojis

--- data/header-nav-links.ts
+++ data/header-nav-links.ts
@@ -1,10 +1,10 @@
 import { SITE_METADATA } from './site-metadata'
 
 export const HEADER_NAV_LINKS = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/snippets', title: 'Snippets' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/blog', title: 'Blog', emoji: 'writing-hand' },
+  { href: '/snippets', title: 'Snippets', emoji: 'dna' },
+  { href: '/projects', title: 'Projects', emoji: 'man-technologist' },
+  { href: '/about', title: 'About', emoji: 'billed-cap' },
 ]
 
 export const MORE_NAV_LINKS = [
