7fca4165de2e8372ec642933669d0b836798b74d
hta218 <huynhtuananh21895@gmail.com>
2024-09-27 15:07:28 +0700

    refactor: update GrowingUnderline component to support dynamic underline length

--- components/ui/growing-underline.tsx
+++ components/ui/growing-underline.tsx
@@ -4,12 +4,14 @@ import type { CSSProperties } from 'react'
 export function GrowingUnderline({
   as: Component = 'span',
   children,
+  active,
   className,
   duration,
   ...rest
 }: {
   children: React.ReactNode
   as?: React.ElementType
+  active?: boolean
   className?: string
   duration?: number
   [key: string]: any
@@ -19,9 +21,11 @@ export function GrowingUnderline({
       className={clsx([
         'bg-gradient-to-r bg-left-bottom bg-no-repeat',
         'transition-[background-size] duration-[var(--duration,300ms)]',
-        'bg-[length:0px_50%] hover:bg-[length:100%_50%]',
         'from-green-200 to-green-100',
         'dark:from-emerald-800 dark:to-emerald-900',
+        active
+          ? 'bg-[length:100%_50%] hover:bg-[length:100%_100%]'
+          : 'bg-[length:0px_50%] hover:bg-[length:100%_50%]',
         className,
       ])}
       style={{ '--duration': `${duration || 300}ms` } as CSSProperties}
