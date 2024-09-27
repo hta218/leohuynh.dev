8110945b590ca3d5baf3c7da717dd1503ccd2b1f
hta218 <huynhtuananh21895@gmail.com>
2024-09-27 15:07:15 +0700

    refactor: optimize book cover component styles

--- components/cards/book/book-cover.tsx
+++ components/cards/book/book-cover.tsx
@@ -8,13 +8,13 @@ export function BookCover({ image, alt }: { image: string; alt: string }) {
         <div
           className={clsx([
             'absolute bottom-1 left-3 top-[3px] w-11/12',
-            'rounded-l-sm rounded-r-md bg-white shadow-book-pages',
+            'rounded-r-md bg-white shadow-book-pages',
             'border border-[gray]',
           ])}
         />
         <div
           className={clsx([
-            'relative rounded-l-sm rounded-r-md leading-[0]',
+            'relative rounded-r-md leading-[0]',
             'transition-all duration-300 ease-in-out',
             '[transform:perspective(2000px)_rotateY(-15deg)_translateX(-10px)_scaleX(0.94)]',
             '[box-shadow:6px_6px_18px_-2px_rgba(0,0,0,0.2),24px_28px_40px_-6px_rgba(0,0,0,0.1)]',
@@ -23,13 +23,7 @@ export function BookCover({ image, alt }: { image: string; alt: string }) {
             'hover:[box-shadow:6px_6px_12px_-1px_rgba(0,0,0,0.1),20px_14px_16px_-6px_rgba(0,0,0,0.1)]',
           ])}
         >
-          <Image
-            src={image}
-            alt={alt}
-            width={1000}
-            height={1500}
-            className="w-full rounded-l-sm rounded-r-md"
-          />
+          <Image src={image} alt={alt} width={1000} height={1500} className="w-full rounded-r-md" />
           <div
             className={clsx([
               'ml-4 h-full w-5',
