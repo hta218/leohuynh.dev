028f841ec21643fa3aa2712504da282155db3d7a
hta218 <huynhtuananh21895@gmail.com>
2024-09-27 23:34:33 +0700

    refactor: update movies-list.tsx to include title type filter and movie count

--- app/movies/movies-list.tsx
+++ app/movies/movies-list.tsx
@@ -5,15 +5,20 @@ import { MovieCard } from '~/components/cards/movie'
 import type { ImdbMovie } from '~/types/data'
 import { RateFilter, RATES, type RateType } from './rate-filter'
 import { Twemoji } from '~/components/ui/twemoji'
+import { TitleTypeFilter, type TitleType } from './title-type-filter'
 
 export function MoviesList({ movies }: { movies: ImdbMovie[] }) {
   let [rate, setRate] = useState<RateType>('10')
+  let [type, setType] = useState<TitleType>('All')
+
   let displayMovies = movies
     .filter((movie) => {
-      if (rate === '<=6') {
-        return Number(movie.your_rating) <= 6
+      if (type === 'All' || type === movie.title_type) {
+        if (rate === '<=6') {
+          return Number(movie.your_rating) <= 6
+        }
+        return movie.your_rating === rate
       }
-      return movie.your_rating === rate
     })
     .sort((m1, m2) => {
       if (m1.your_rating === m2.your_rating) {
@@ -32,14 +37,23 @@ export function MoviesList({ movies }: { movies: ImdbMovie[] }) {
             ({displayMovies.length} titles)
           </span>
         </div>
-        <div className="flex items-center gap-3">
-          My rate: <RateFilter rate={rate} setRate={setRate} />
+        <div className="flex gap-5">
+          <div className="flex items-center gap-2">
+            Type: <TitleTypeFilter type={type} setType={setType} />
+          </div>
+          <div className="flex items-center gap-2">
+            My rate: <RateFilter rate={rate} setRate={setRate} />
+          </div>
         </div>
       </div>
       <div className="grid grid-cols-1 gap-4 gap-x-6 gap-y-[4.5rem] pt-16 md:grid-cols-2 md:gap-y-20">
-        {displayMovies.map((movie) => {
-          return <MovieCard movie={movie} key={movie.const} />
-        })}
+        {displayMovies.length ? (
+          displayMovies.map((movie) => {
+            return <MovieCard movie={movie} key={movie.const} />
+          })
+        ) : (
+          <div className="text-base">No movies found</div>
+        )}
       </div>
     </div>
   )
