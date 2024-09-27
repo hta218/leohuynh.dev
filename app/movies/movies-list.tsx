add9689ddbb93f8f335905419f8371a4657d34f4
hta218 <huynhtuananh21895@gmail.com>
2024-09-27 17:56:32 +0700

    refactor: update movies-list.tsx to include emoji and movie count

--- app/movies/movies-list.tsx
+++ app/movies/movies-list.tsx
@@ -3,7 +3,8 @@
 import { useState } from 'react'
 import { MovieCard } from '~/components/cards/movie'
 import type { ImdbMovie } from '~/types/data'
-import { RateFilter, type RateType } from './rate-filter'
+import { RateFilter, RATES, type RateType } from './rate-filter'
+import { Twemoji } from '~/components/ui/twemoji'
 
 export function MoviesList({ movies }: { movies: ImdbMovie[] }) {
   let [rate, setRate] = useState<RateType>('10')
@@ -20,11 +21,20 @@ export function MoviesList({ movies }: { movies: ImdbMovie[] }) {
       }
       return Number(m2.your_rating) - Number(m1.your_rating)
     })
+  let { description, emoji } = RATES.find(({ value }) => value === rate) || RATES[0]
 
   return (
     <div className="space-y-4 md:space-y-6">
-      <div className="flex items-center justify-end gap-4">
-        My rate: <RateFilter rate={rate} setRate={setRate} />
+      <div className="flex items-center justify-between gap-4">
+        <div className="flex items-center gap-2 text-xl font-medium">
+          <Twemoji emoji={emoji} /> {description}{' '}
+          <span className="font-normal text-gray-600 dark:text-gray-400">
+            ({displayMovies.length} titles)
+          </span>
+        </div>
+        <div className="flex items-center gap-3">
+          My rate: <RateFilter rate={rate} setRate={setRate} />
+        </div>
       </div>
       <div className="grid grid-cols-1 gap-4 gap-x-6 gap-y-[4.5rem] pt-16 md:grid-cols-2 md:gap-y-20">
         {displayMovies.map((movie) => {
