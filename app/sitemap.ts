a7861838ee0ab9ff4f68a16c033dd1f31ddcd92d
hta218 <huynhtuananh21895@gmail.com>
2024-09-30 22:18:27 +0700

    refactor: update sitemap to include snippet routes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,22 +1,28 @@
-import { allBlogs } from 'contentlayer/generated'
+import { allBlogs, allSnippets } from 'contentlayer/generated'
 import type { MetadataRoute } from 'next'
 import { SITE_METADATA } from '~/data/site-metadata'
 
-// TODO: update sitemap
 export default function sitemap(): MetadataRoute.Sitemap {
-  const siteUrl = SITE_METADATA.siteUrl
-
-  const blogRoutes = allBlogs
-    .filter((post) => !post.draft)
-    .map((post) => ({
-      url: `${siteUrl}/${post.path}`,
-      lastModified: post.lastmod || post.date,
+  let siteUrl = SITE_METADATA.siteUrl
+  let blogRoutes = allBlogs
+    .filter((p) => !p.draft)
+    .map(({ path, lastmod, date }) => ({
+      url: `${siteUrl}/${path}`,
+      lastModified: lastmod || date,
+    }))
+  let snippetRoutes = allSnippets
+    .filter((s) => !s.draft)
+    .map(({ path, lastmod, date }) => ({
+      url: `${siteUrl}/snippets/${path}`,
+      lastModified: lastmod || date,
     }))
 
-  const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
-    url: `${siteUrl}/${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
+  let routes = ['', 'blog', 'snippets', 'projects', 'about', 'books', 'movies', 'tags'].map(
+    (route) => ({
+      url: `${siteUrl}/${route}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    })
+  )
 
-  return [...routes, ...blogRoutes]
+  return [...routes, ...blogRoutes, ...snippetRoutes]
 }
