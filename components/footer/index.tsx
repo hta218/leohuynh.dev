73ca2978e6c2af10c9d179dfefaf74a4ff36803a
hta218 <huynhtuananh21895@gmail.com>
2024-09-30 21:36:51 +0700

    refactor: update footer component with new layout and additional features

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,21 +1,46 @@
-import { Link } from '~/components/ui/link'
+import { clsx } from 'clsx'
 import { Container } from '~/components/ui/container'
+import { Link } from '~/components/ui/link'
 import { SITE_METADATA } from '~/data/site-metadata'
-import MadeInVietNameIcon from '~/icons/miv.svg'
-import { BuiltWith } from './built-with'
+import MadeInVietNam from '~/icons/miv.svg'
+import { SpotifyNowPlaying } from '../cards/profile/now-playing'
+import { AddressAndTime } from './address-and-time'
+import { FooterNav } from './footer-nav'
+import { LogoAndRepo } from './logo-and-repo'
+import { Signature } from './signature'
 
 export function Footer() {
   return (
-    <Container as="footer" className="mt-8">
-      <div className="items-center justify-between space-y-4 border-t border-gray-200 py-8 dark:border-gray-700 lg:flex lg:space-y-0">
-        <BuiltWith />
+    <Container as="footer" className="my-8 md:mt-16">
+      <div
+        className={clsx([
+          'grid grid-cols-1 gap-x-8 gap-y-8 py-8 md:grid-cols-2',
+          'border-t border-gray-200 dark:border-gray-700',
+        ])}
+      >
+        <div className="col-span-1 space-y-4">
+          <LogoAndRepo />
+          <div className="italic text-gray-500 dark:text-gray-400">{SITE_METADATA.description}</div>
+          <div className="pt-4">
+            <div className="flex gap-20">
+              <Signature className="h-20 w-40" />
+              <AddressAndTime />
+            </div>
+          </div>
+        </div>
+        <div className="space-y-4">
+          <FooterNav />
+        </div>
+      </div>
+      <div className="flex items-center justify-between gap-4 border-t border-gray-200 pt-4 dark:border-gray-700">
+        <SpotifyNowPlaying
+          className={clsx(['[--artist-color:theme(colors.gray.500)]'])}
+          songEffect="underline"
+        />
         <div className="my-2 flex flex-wrap items-center gap-1.5 text-gray-500 dark:text-gray-400">
-          <span className="hidden md:block">{`© ${new Date().getFullYear()}`}</span>
-          <span className="hidden md:block">-</span>
-          <span>{SITE_METADATA.title}</span>
           <Link href={SITE_METADATA.siteRepo}>
             <span data-umami-event="made-in-vietnam">
-              <MadeInVietNameIcon />
+              <MadeInVietNam />
             </span>
           </Link>
         </div>
