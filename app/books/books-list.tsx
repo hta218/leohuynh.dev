3e87360f140fc9f647983a3ce51452372d68d580
hta218 <huynhtuananh21895@gmail.com>
2024-10-08 23:38:03 +0700

    refactor: update books route to add select shelf functionality

---
+++ app/books/books-list.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useState } from 'react'
+import { BookCard } from '~/components/cards/book'
+import type { GoodreadsBook } from '~/types/data'
+import { ShelveSelect, type Shelf } from './shelve-select'
+
+export function BooksList({ books }: { books: GoodreadsBook[] }) {
+  let [shelf, setShelf] = useState<Shelf>('all')
+  let displayBooks =
+    shelf === 'all'
+      ? books
+      : books.filter((book) => {
+          if (shelf === 'read') {
+            return book.user_shelves === ''
+          }
+          return book.user_shelves.includes(shelf)
+        })
+
+  return (
+    <div className="py-5 md:py-10">
+      <div className="mb-6 flex items-center justify-between gap-4">
+        <span className="text-2xl font-bold leading-9 tracking-tight">
+          <span className="mr-2 capitalize">{shelf}</span>
+          <span className="font-normal">({displayBooks.length})</span>
+        </span>
+        <div className="flex items-center gap-2">
+          <span>Shelve: </span>
+          <ShelveSelect shelf={shelf} setShelf={setShelf} />
+        </div>
+      </div>
+      <div className="space-y-10">
+        {displayBooks.map((book) => (
+          <BookCard key={book.guid} book={book} />
+        ))}
+      </div>
+    </div>
+  )
+}
--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,5 +1,4 @@
 import { genPageMetadata } from '~/app/seo'
-import { BookCard } from '~/components/cards/book'
 import { Container } from '~/components/ui/container'
 import { GrowingUnderline } from '~/components/ui/growing-underline'
 import { Image } from '~/components/ui/image'
@@ -8,22 +7,11 @@ import { PageHeader } from '~/components/ui/page-header'
 import { SITE_METADATA } from '~/data/site-metadata'
 import books from '~/json/books.json' assert { type: 'json' }
 import type { GoodreadsBook } from '~/types/data'
+import { BooksList } from './books-list'
 
 export let metadata = genPageMetadata({ title: 'My bookshelf' })
 
 export default async function BooksPage() {
-  let readingBooks: GoodreadsBook[] = []
-  let readBooks: GoodreadsBook[] = []
-  for (let book of books as unknown as GoodreadsBook[]) {
-    if (book.user_shelves === 'currently-reading') {
-      readingBooks.push(book)
-      readingBooks.sort((a, b) => Number(b.user_rating) - Number(a.user_rating))
-    } else {
-      readBooks.push(book)
-      readBooks.sort((a, b) => Number(b.user_rating) - Number(a.user_rating))
-    }
-  }
-
   return (
     <Container className="pt-4 lg:pt-12">
       <PageHeader
@@ -50,26 +38,13 @@ export default async function BooksPage() {
         }
         className="border-b border-gray-200 dark:border-gray-700"
       />
-      <div className="py-5 md:py-10">
-        <h3 className="mb-6 text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:text-3xl">
-          Currently reading <span className="font-normal">({readingBooks.length})</span>
-        </h3>
-        <div className="space-y-10">
-          {readingBooks.map((book) => (
-            <BookCard key={book.guid} book={book} />
-          ))}
-        </div>
-      </div>
-      <div className="mt-6 border-t border-gray-200 py-5 dark:border-gray-700 md:mt-10 md:py-10">
-        <h3 className="mb-6 text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:text-3xl">
-          Finished <span className="font-normal">({readBooks.length})</span>
-        </h3>
-        <div className="space-y-10">
-          {readBooks.map((book) => (
-            <BookCard key={book.guid} book={book} />
-          ))}
-        </div>
-      </div>
+      <BooksList
+        books={
+          books.sort(
+            (a, b) => Number(b.user_rating) - Number(a.user_rating)
+          ) as unknown as GoodreadsBook[]
+        }
+      />
       <div className="mt-6 border-t border-gray-200 py-5 dark:border-gray-700 md:mt-10 md:py-10">
         <h3 className="mb-6 text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:text-3xl">
           FYI
