48f57cbaa62ef04c339c68d95660126d3ed9ab4a
hta218 <huynhtuananh21895@gmail.com>
2024-10-08 23:37:29 +0700

    feat: add ShelveSelect component for filtering book shelves

---
+++ app/books/shelve-select.tsx
@@ -0,0 +1,94 @@
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
+import clsx from 'clsx'
+import { ChevronDown } from 'lucide-react'
+import { Fragment } from 'react'
+
+const SHELVES: {
+  label: string
+  value: Shelf
+}[] = [
+  {
+    label: 'All',
+    value: 'all',
+  },
+  {
+    label: 'Currently reading',
+    value: 'currently-reading',
+  },
+  {
+    label: 'Read',
+    value: 'read',
+  },
+  {
+    label: 'Abandoned',
+    value: 'abandoned',
+  },
+]
+
+export type Shelf = 'all' | 'currently-reading' | 'read' | 'abandoned'
+
+export function ShelveSelect({
+  shelf,
+  setShelf,
+}: {
+  shelf: Shelf
+  setShelf: React.Dispatch<React.SetStateAction<Shelf>>
+}) {
+  let { label, value: selectedValue } = SHELVES.find(({ value }) => value === shelf) || SHELVES[0]
+
+  return (
+    <div className="flex items-center">
+      <Menu as="div" className="relative inline-block text-left">
+        <MenuButton
+          aria-label="More links"
+          className="inline-flex items-center gap-2 rounded-lg border border-gray-200 px-3 py-1.5 font-medium dark:border-gray-700"
+          data-umami-event="movies-rate-filter"
+        >
+          <span>{label}</span>
+          <ChevronDown strokeWidth={1.5} size={20} />
+        </MenuButton>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <MenuItems
+            className={clsx([
+              'absolute left-0 z-50 md:left-auto md:right-0',
+              'mt-2 w-40 origin-top-right rounded-md text-right shadow-lg',
+              'bg-white dark:bg-black',
+              'ring-1 ring-black ring-opacity-5 focus:outline-none',
+            ])}
+          >
+            <div className="space-y-1 p-1">
+              {SHELVES.map(({ label, value }) => (
+                <MenuItem key={value} as="div">
+                  {({ close }) => (
+                    <button
+                      className={clsx([
+                        'flex w-full items-center gap-2 rounded-md px-2 py-1.5',
+                        value === selectedValue
+                          ? 'bg-gray-200 dark:bg-gray-800'
+                          : 'hover:bg-gray-200 dark:hover:bg-gray-800',
+                      ])}
+                      onClick={() => {
+                        setShelf(value)
+                        close()
+                      }}
+                    >
+                      <span>{label}</span>
+                    </button>
+                  )}
+                </MenuItem>
+              ))}
+            </div>
+          </MenuItems>
+        </Transition>
+      </Menu>
+    </div>
+  )
+}
