c4be1aacbbaa8c042cbfb26eaea744956e1a6e1a
hta218 <huynhtuananh21895@gmail.com>
2024-10-09 23:30:43 +0700

    fix: update `frame-src` in CSP to include *.github.io

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src giscus.app *.github.io
 `
 
 const securityHeaders = [
