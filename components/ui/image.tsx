c7089437f57f9fa9926d6d01cc7ee5ad847854db
hta218 <hta218.dev@gmail.com>
2024-10-12 00:03:20 +0700

    feat: enhance `Image` component with blur loading effect

--- components/ui/image.tsx
+++ components/ui/image.tsx
@@ -1,8 +1,56 @@
-import type { ImageProps } from 'next/image'
+'use client'
+
+import { clsx } from 'clsx'
+import type { ImageProps as NextImageProps } from 'next/image'
 import NextImage from 'next/image'
+import { usePathname } from 'next/navigation'
+import { useState } from 'react'
 
-const basePath = process.env.BASE_PATH
+let loadedImages: string[] = []
 
-export function Image({ src, ...rest }: ImageProps) {
-  return <NextImage src={`${basePath || ''}${src}`} {...rest} />
+function useImageLoadedState(src: string) {
+  let pathname = usePathname()
+  let uniqueImagePath = pathname + '__' + src
+  let [loaded, setLoaded] = useState(() => loadedImages.includes(uniqueImagePath))
+  return [
+    loaded,
+    () => {
+      if (loaded) return
+      loadedImages.push(uniqueImagePath)
+      setLoaded(true)
+    },
+  ] as const
+}
+
+interface ImageProps extends Omit<NextImageProps, 'priority'> {}
+
+export function Image(props: ImageProps) {
+  let { alt, src, loading = 'lazy', style = { objectFit: 'cover' }, className, ...rest } = props
+  let [loaded, onLoad] = useImageLoadedState(src as string)
+
+  return (
+    <div
+      className={clsx(
+        'overflow-hidden',
+        !loaded && 'animate-pulse [animation-duration:4s]',
+        className
+      )}
+    >
+      <NextImage
+        className={clsx(
+          '[transition:filter_500ms_cubic-bezier(.4,0,.2,1)]',
+          'my-0 h-full max-h-full w-full object-center',
+          loaded ? 'blur-0' : 'blur-xl'
+        )}
+        src={src}
+        alt={alt}
+        style={style}
+        loading={loading}
+        priority={loading === 'eager'}
+        quality={100}
+        onLoad={onLoad}
+        {...rest}
+      />
+    </div>
+  )
 }
