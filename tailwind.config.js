360e98eecfdf6e0b8e6750a3d12dce2f0e23ede6
hta218 <hta218.dev@gmail.com>
2024-10-11 13:29:52 +0700

    fix(tailwind): update primary color for links and code elements in invert mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -145,11 +145,14 @@ module.exports = {
         invert: {
           css: {
             a: {
-              color: theme('colors.primary.500'),
+              color: theme('colors.primary.400'),
               '&:hover': {
                 color: `${theme('colors.primary.400')}`,
               },
               code: { color: theme('colors.primary.400') },
+            },
+            code: {
+              color: theme('colors.primary.400'),
             },
             'h1,h2,h3,h4,h5,h6': {
               color: theme('colors.gray.100'),
