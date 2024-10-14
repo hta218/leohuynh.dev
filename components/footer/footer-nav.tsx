e3f66ed46614aea9a5866a6bd2757bae710ca7d1
hta218 <hta218.dev@gmail.com>
2024-10-14 22:58:26 +0700

    fix: update footer navigation layout for improved responsiveness and spacing of links

--- components/footer/footer-nav.tsx
+++ components/footer/footer-nav.tsx
@@ -1,32 +1,43 @@
 import { ExternalLink } from 'lucide-react'
+import { Fragment } from 'react'
 import { GrowingUnderline } from '~/components/ui/growing-underline'
 import { Link } from '~/components/ui/link'
 import { FOOTER_NAV_LINKS, FOOTER_PERSONAL_STUFF } from '~/data/navigation'
 
 export function FooterNav() {
   return (
-    <div className="flex gap-24 px-1 md:justify-end md:px-0 md:text-right">
-      <div className="space-y-4">
+    <div className="flex flex-col gap-4 px-1 md:flex-row md:justify-end md:gap-24 md:px-0 md:text-right">
+      <div className="space-y-1 md:space-y-4">
         <div className="flex h-11 items-center font-semibold md:justify-end">
           <span>Sitemap</span>
         </div>
-        <ul className="space-y-3">
-          {FOOTER_NAV_LINKS.map((link) => (
-            <li key={link.title}>
-              <FooterLink link={link} />
-            </li>
+        <ul className="flex gap-4 md:flex-col md:gap-3">
+          {FOOTER_NAV_LINKS.map((link, idx) => (
+            <Fragment key={link.title}>
+              <li>
+                <FooterLink link={link} />
+              </li>
+              {idx !== FOOTER_NAV_LINKS.length - 1 && (
+                <span className="text-gray-400 md:hidden">/</span>
+              )}
+            </Fragment>
           ))}
         </ul>
       </div>
-      <div className="space-y-4">
+      <div className="space-y-1 md:space-y-4">
         <div className="flex h-11 items-center font-semibold">
           <span>Personal stuff</span>
         </div>
-        <ul className="space-y-3">
-          {FOOTER_PERSONAL_STUFF.map((link) => (
-            <li key={link.title}>
-              <FooterLink link={link} />
-            </li>
+        <ul className="flex gap-4 md:flex-col md:gap-3">
+          {FOOTER_PERSONAL_STUFF.map((link, idx) => (
+            <Fragment key={link.title}>
+              <li>
+                <FooterLink link={link} />
+              </li>
+              {idx !== FOOTER_NAV_LINKS.length - 1 && (
+                <span className="text-gray-400 md:hidden">/</span>
+              )}
+            </Fragment>
           ))}
         </ul>
       </div>
