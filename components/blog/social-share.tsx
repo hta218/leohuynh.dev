a9cf588650aa743d077c1b8f16085c2868d087a7
hta218 <hta218.dev@gmail.com>
2024-10-18 15:05:54 +0700

    chore: simplify social share buttons

--- components/blog/social-share.tsx
+++ components/blog/social-share.tsx
@@ -1,71 +1,42 @@
 'use client'
 
-import { Facebook } from 'lucide-react'
-import { FacebookShareButton, TwitterShareButton } from 'react-share'
-import { Link } from '~/components/ui/link'
-import { GrowingUnderline } from '~/components/ui/growing-underline'
+import { clsx } from 'clsx'
+import { Facebook, Linkedin } from 'lucide-react'
+import { FacebookShareButton, LinkedinShareButton, TwitterShareButton } from 'react-share'
 import { SITE_METADATA } from '~/data/site-metadata'
 import XIcon from '~/icons/x.svg'
 
 type SocialButtonsProps = {
   postUrl: string
   title: string
-  filePath: string
+  className?: string
 }
 
-export function SocialShare({ postUrl, title, filePath }: SocialButtonsProps) {
-  let CREATE_DISCUS_ON_TWITTER = process.env.NEXT_PUBLIC_CREATE_DISCUS_ON_TWITTER
-  let CREATE_DISCUS_ON_GITHUB = process.env.NEXT_PUBLIC_CREATE_DISCUS_ON_GITHUB
-  let SHARE_ON_TWITTER = process.env.NEXT_PUBLIC_SHARE_ON_TWITTER
-  let SHARE_ON_FACEBOOK = process.env.NEXT_PUBLIC_SHARE_ON_FACEBOOK
-
+export function SocialShare({ postUrl, title, className }: SocialButtonsProps) {
   return (
-    <div className="flex flex-col items-center justify-center gap-6 py-6 text-sm text-gray-700 dark:text-gray-300 md:flex-row md:justify-between">
-      <div className="space-x-1">
-        {CREATE_DISCUS_ON_TWITTER === 'TRUE' && (
-          <>
-            <Link
-              href={`https://twitter.com/search?q=${encodeURIComponent(postUrl)}`}
-              rel="nofollow"
-            >
-              <GrowingUnderline data-umami-event="discuss-on-x">
-                Discuss on <span className="font-semibold">X</span> (
-                <span className="font-semibold">Twitter</span>)
-              </GrowingUnderline>
-            </Link>
-            <span>{` • `}</span>
-          </>
-        )}
-        {CREATE_DISCUS_ON_GITHUB === 'TRUE' && (
-          <Link href={`${SITE_METADATA.siteRepo}/blob/main/data/${filePath}?plain=1`}>
-            <GrowingUnderline data-umami-event="view-on-github">
-              Edit on <span className="font-semibold">GitHub</span>
-            </GrowingUnderline>
-          </Link>
-        )}
-      </div>
-      <div className="flex items-center text-white">
-        {SHARE_ON_TWITTER === 'TRUE' && (
-          <TwitterShareButton
-            url={postUrl}
-            title={title}
-            via={SITE_METADATA.x}
-            className="mr-2 flex items-center overflow-hidden rounded !bg-x !px-2.5 !py-1.5 hover:opacity-90"
-          >
-            <XIcon className="h-5 w-5" fill="#fff" viewBox="0 0 1200 1227" />
-            <span className="ml-2.5 mr-1.5 font-extrabold text-white">Tweet</span>
-          </TwitterShareButton>
-        )}
-        {SHARE_ON_FACEBOOK === 'TRUE' && (
-          <FacebookShareButton
-            url={postUrl}
-            className="mr-2 flex items-center overflow-hidden rounded !bg-facebook !px-2.5 !py-1.5 hover:opacity-90"
-          >
-            <Facebook strokeWidth={1.5} size={20} />
-            <span className="ml-2.5 mr-1.5 font-extrabold text-white">Share</span>
-          </FacebookShareButton>
-        )}
-      </div>
+    <div className={clsx('flex items-center gap-2 text-white', className)}>
+      <span className="text-gray-500">Share:</span>
+      <TwitterShareButton
+        url={postUrl}
+        title={title}
+        via={SITE_METADATA.x}
+        className="flex items-center overflow-hidden rounded-full !bg-x !px-2 !py-2 text-white hover:opacity-90"
+      >
+        <XIcon className="h-3.5 w-3.5" fill="#fff" viewBox="0 0 1200 1227" />
+      </TwitterShareButton>
+      <LinkedinShareButton
+        url={postUrl}
+        title={title}
+        className="flex items-center overflow-hidden rounded-full !bg-linkedin !px-2 !py-2 text-white hover:opacity-90"
+      >
+        <Linkedin strokeWidth={1.5} size={16} />
+      </LinkedinShareButton>
+      <FacebookShareButton
+        url={postUrl}
+        className="flex items-center overflow-hidden rounded-full !bg-facebook !px-2 !py-2 text-white hover:opacity-90"
+      >
+        <Facebook strokeWidth={1.5} size={16} />
+      </FacebookShareButton>
     </div>
   )
 }
