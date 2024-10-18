1afa020f37dcce1e744e1ad66da080c74818beac
hta218 <hta218.dev@gmail.com>
2024-10-18 12:29:39 +0700

    feat: implement function to extract TOC headings from markdown files and store in file data

---
+++ server/remark-toc-headings.server.ts
@@ -0,0 +1,42 @@
+import { slug } from 'github-slugger'
+import { toString } from 'mdast-util-to-string'
+import { remark } from 'remark'
+import { visit } from 'unist-util-visit'
+
+export type TocItem = {
+  value: string
+  url: string
+  depth: number
+}
+
+export type Toc = TocItem[]
+
+/**
+ * Extracts TOC headings from markdown file and adds it to the file's data object.
+ */
+function remarkTocHeadings() {
+  return (tree, file) => {
+    let toc: Toc = []
+    visit(tree, 'heading', (node) => {
+      let textContent = toString(node).replace(/<[^>]*(>|$)/g, '')
+      toc.push({
+        value: textContent,
+        url: '#' + slug(textContent),
+        depth: node.depth,
+      })
+    })
+    file.data.toc = toc
+  }
+}
+
+/**
+ * Passes markdown file through remark to extract TOC headings
+ *
+ * @param {string} markdown
+ * @return {*}  {Promise<Toc>}
+ */
+export async function extractTocHeadings(markdown: string): Promise<Toc> {
+  let vfile = await remark().use(remarkTocHeadings).process(markdown)
+  // @ts-ignore
+  return vfile.data.toc
+}
