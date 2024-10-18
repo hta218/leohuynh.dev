baee00784eab634fba64a8e32e4c1846ba577c99
hta218 <hta218.dev@gmail.com>
2024-10-18 22:30:42 +0700

    feat: enhance Banner component with credit display

--- components/blog/banner.tsx
+++ components/blog/banner.tsx
@@ -1,28 +1,54 @@
 import { clsx } from 'clsx'
 import { GritBackground } from '~/components/ui/grit-background'
 import { Image, Zoom } from '~/components/ui/image'
+import { capitalize, kebabCaseToPlainText } from '~/utils/misc'
+import { Link } from '../ui/link'
 
-export function Banner({
-  banner,
-  title,
-  className,
-}: {
-  banner: string
-  title: string
-  className?: string
-}) {
+export function Banner({ banner, className }: { banner: string; className?: string }) {
+  let [path, author, id] = banner.split('__')
+  let handle = path.split('/').pop() || ''
   return (
     <div className={clsx('relative', className)}>
       <Zoom>
         <Image
           src={banner}
-          alt={title}
+          alt={capitalize(kebabCaseToPlainText(handle)) || 'Article banner photo'}
           width={1600}
-          height={1200}
+          height={900}
           className="h-auto w-full rounded-lg"
         />
       </Zoom>
       <GritBackground className="inset-0 rounded-lg opacity-75" />
     </div>
   )
+}
+
+interface CreditProps {
+  author: string
+  id: string
+  className?: string
+}
+
+function Credit({ author, id, className }: CreditProps) {
+  if (author && id) {
+    return (
+      <div className={clsx('italic', className)}>
+        Photo by{' '}
+        <Link
+          className="underline-offset-4 hover:underline"
+          href={`https://unsplash.com/@${author}`}
+        >
+          <span data-umami-event="banner-author">@{author}</span>
+        </Link>{' '}
+        on{' '}
+        <Link
+          className="underline-offset-4 hover:underline"
+          href={`https://unsplash.com/photos/${id}`}
+        >
+          <span data-umami-event="banner-unsplash">Unsplash</span>
+        </Link>
+      </div>
+    )
+  }
+  return null
 }
