855429b18191a6f2b25ce216fbe5abafda24fc8b
hta218 <hta218.dev@gmail.com>
2024-10-18 15:04:56 +0700

    refactor: update blog meta

--- components/blog/blog-meta.tsx
+++ components/blog/blog-meta.tsx
@@ -11,27 +11,33 @@ type BlogMetaProps = {
 
 export function BlogMeta({ date, lastmod, slug, readingTime }: BlogMetaProps) {
   return (
-    <dd className="flex flex-wrap text-sm font-medium leading-6 text-gray-500 dark:text-gray-400 md:text-base">
-      <time dateTime={date} className="flex items-center justify-center">
-        <Twemoji emoji="calendar" size="base" />
-        <span className="ml-1.5 md:ml-2">{formatDate(date)}</span>
-      </time>
-      {lastmod && (
-        <time dateTime={date} className="ml-1 flex items-center justify-center">
-          (<span>updated on</span>
-          <span className="ml-1.5 md:ml-2">{formatDate(lastmod)}</span>)
+    <dl>
+      <dt className="sr-only">Published on</dt>
+      <dd className="flex flex-wrap items-center gap-3 text-sm font-medium leading-6 text-gray-500 dark:text-gray-400 md:text-base">
+        <time dateTime={date} className="flex items-center justify-center">
+          <Twemoji emoji="calendar" size="base" />
+          <span className="ml-1.5 md:ml-2">{formatDate(date)}</span>
         </time>
-      )}
-      <span className="mx-2">{` • `}</span>
-      <div className="flex items-center">
-        <Twemoji emoji="hourglass-not-done" size="base" />
-        <span className="ml-1.5 md:ml-2">{Math.ceil(readingTime.minutes)} mins read</span>
-      </div>
-      {/* <span className="mx-2">{` • `}</span>
+        {lastmod && (
+          <>
+            <span className="text-gray-400">/</span>
+            <time dateTime={date} className="flex items-center justify-center">
+              <span>Updated</span>
+              <span className="ml-1.5 md:ml-2">{formatDate(lastmod)}</span>
+            </time>
+          </>
+        )}
+        <span className="text-gray-400">/</span>
+        <div className="flex items-center">
+          <Twemoji emoji="three-o-clock" size="base" />
+          <span className="ml-1.5 md:ml-2">{Math.ceil(readingTime.minutes)} mins read</span>
+        </div>
+        {/* <span className="mx-2 text-gray-500">|</span>
       <div className="flex items-center">
         <Twemoji emoji="eye" size="base" />
         <ViewCounter className="ml-1.5 md:ml-2" slug={slug} />
       </div> */}
-    </dd>
+      </dd>
+    </dl>
   )
 }
