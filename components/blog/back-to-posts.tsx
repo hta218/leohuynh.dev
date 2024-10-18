cb8d77e47d68788d5f48870fa8da5768fca95758
hta218 <hta218.dev@gmail.com>
2024-10-18 15:04:26 +0700

    refactor: enhance BackToPosts with label and add improved styling

--- components/blog/back-to-posts.tsx
+++ components/blog/back-to-posts.tsx
@@ -1,11 +1,14 @@
 import { MoveLeft } from 'lucide-react'
 import { Link } from '~/components/ui/link'
+import { GrowingUnderline } from '../ui/growing-underline'
 
-export function BackToPosts() {
+export function BackToPosts({ label, className }: { label: string; className?: string }) {
   return (
-    <Link href="/blog" className="flex w-fit gap-3">
-      <MoveLeft strokeWidth={1} />
-      <span>Back to the blog</span>
-    </Link>
+    <div className={className}>
+      <Link href="/blog" className="flex w-fit items-center gap-3">
+        <MoveLeft strokeWidth={1.5} />
+        <GrowingUnderline data-umami-event="discuss-on-x">{label}</GrowingUnderline>
+      </Link>
+    </div>
   )
 }
