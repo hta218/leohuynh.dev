56b187c17a804e8573e312ed02562f5146f6abd9
hta218 <hta218.dev@gmail.com>
2024-10-19 15:22:08 +0700

    chore: update umami event data attributes for better readability in career timeline component

--- components/author/career.tsx
+++ components/author/career.tsx
@@ -241,7 +241,7 @@ function TimelineItem({ exp, last }: { exp: (typeof EXPERIENCES)[0]; last?: bool
               'transition-transform duration-300 ease-in-out',
               'text-gray-600 dark:text-gray-500',
             ])}
-            data-umami-event={event + 'expand'}
+            data-umami-event={`${event} expand`}
           />
           <Minus
             size={18}
@@ -251,7 +251,7 @@ function TimelineItem({ exp, last }: { exp: (typeof EXPERIENCES)[0]; last?: bool
               'transition-transform duration-300 ease-in-out',
               'text-gray-600 dark:text-gray-500',
             ])}
-            data-umami-event={event + 'collapse'}
+            data-umami-event={`${event} collapse`}
           />
           <div className="flex flex-col">
             <div className="line-clamp-1 text-xs tabular-nums text-gray-500 dark:text-gray-400">
