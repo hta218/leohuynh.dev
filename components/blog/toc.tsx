09494d774faba5f0187e33880b60a66076f281cc
hta218 <hta218.dev@gmail.com>
2024-10-22 21:40:14 +0700

    refactor: clean up `TableOfContents`

--- components/blog/toc.tsx
+++ components/blog/toc.tsx
@@ -1,7 +1,4 @@
-'use client'
-
 import { clsx } from 'clsx'
-import { useEffect, useRef, useState } from 'react'
 import { Link } from '~/components/ui/link'
 
 type TocItem = {
@@ -10,44 +7,7 @@ type TocItem = {
   depth: number
 }
 
-function useActiveTocItem(ids: string[]) {
-  let [inViewIds, setInViewIds] = useState<string[]>([])
-  let observer = useRef<IntersectionObserver | null>(null)
-
-  useEffect(() => {
-    if (document) {
-      let headings = ids.map((id) => document.querySelector(id))
-      observer.current?.disconnect()
-      observer.current = new IntersectionObserver(
-        (entries) => {
-          for (let { intersectionRatio, target } of entries) {
-            if (intersectionRatio > 0) {
-              if (!inViewIds.includes(target.id)) {
-                setInViewIds([...inViewIds, target.id])
-              }
-            } else {
-              if (inViewIds.includes(target.id) && inViewIds.length > 1) {
-                setInViewIds(inViewIds.filter((id) => id !== target.id))
-              }
-            }
-          }
-        },
-        { rootMargin: '-96px 0% 0% 0%' }
-      )
-      for (let el of headings) {
-        el && observer.current.observe(el)
-      }
-      return () => observer.current?.disconnect()
-    }
-  }, [ids])
-
-  return inViewIds[0]
-}
-
 export function TableOfContents({ toc, className }: { toc: TocItem[]; className?: string }) {
-  let ids = toc.map((item) => item.url)
-  let activeId = useActiveTocItem(ids)
-
   return (
     <div className={clsx('space-y-4', className)}>
       <h3 className="text-2xl font-semibold">On this page</h3>
@@ -55,12 +15,7 @@ export function TableOfContents({ toc, className }: { toc: TocItem[]; className?
         {toc.map(({ value, depth, url }) => (
           <li
             key={url}
-            className={clsx([
-              'font-medium',
-              url === `#${activeId}`
-                ? 'text-gray-700 dark:text-gray-200'
-                : 'text-gray-400 hover:text-gray-700 dark:text-gray-500 dark:hover:text-gray-200',
-            ])}
+            className="font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-200"
             style={{ paddingLeft: (depth - 2) * 16 }}
           >
             <Link href={url}>{value}</Link>
