fdeb1b2e6259ad739d6f4badeba39610e5043090
hta218 <hta218.dev@gmail.com>
2024-10-30 16:42:08 +0700

    feat(db): implement functions to get and update blog statistics in the database

---
+++ db/queries.ts
@@ -0,0 +1,28 @@
+import { eq, and } from 'drizzle-orm'
+import { db } from './index'
+import { statsTable, type StatsType } from './schema'
+
+export async function getBlogStats(type: StatsType, slug: string) {
+  let stats = await db
+    .select()
+    .from(statsTable)
+    .where(and(eq(statsTable.type, type), eq(statsTable.slug, slug)))
+  if (stats.length) {
+    return stats[0]
+  }
+  let newStats = await db.insert(statsTable).values({ type, slug }).returning()
+  return newStats[0]
+}
+
+export async function updateBlogStats(
+  type: StatsType,
+  slug: string,
+  updates: { [key: string]: any }
+) {
+  let updatedStats = await db
+    .update(statsTable)
+    .set(updates)
+    .where(and(eq(statsTable.type, type), eq(statsTable.slug, slug)))
+    .returning()
+  return updatedStats[0]
+}
