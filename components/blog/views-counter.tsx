e4edf4a9c0422b7100d62cbe9df7147236d00431
hta218 <hta218.dev@gmail.com>
2024-10-30 21:22:49 +0700

    feat(blog): add ViewsCounter component for displaying and updating view counts in blog posts

---
+++ components/blog/views-counter.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useEffect } from 'react'
+import type { StatsType } from '~/db/schema'
+import { useBlogStats, useUpdateBlogStats } from '~/hooks/use-blog-stats'
+
+export function ViewsCounter({
+  type,
+  slug,
+  className,
+}: {
+  type: StatsType
+  slug: string
+  className?: string
+}) {
+  let [stats, isLoading] = useBlogStats(type, slug)
+  let updateView = useUpdateBlogStats()
+
+  useEffect(() => {
+    if (!isLoading && stats) {
+      updateView({ type, slug, views: stats['views'] + 1 })
+    }
+  }, [stats, isLoading])
+
+  return <span className={className}>{isLoading ? '---' : stats['views'] + ' views'}</span>
+}
