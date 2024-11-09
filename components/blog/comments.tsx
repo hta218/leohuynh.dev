5fa7eea0bfe3d5b8f80e94cdfa20cbab447cfe47
hta218 <hta218.dev@gmail.com>
2024-11-09 16:05:07 +0700

    feat: implement Giscus comments integration

--- components/blog/comments.tsx
+++ components/blog/comments.tsx
@@ -1,15 +1,71 @@
 'use client'
 
-import { Comments as CommentsComponent, type CommentsConfig } from 'pliny/comments'
+import { useTheme } from 'next-themes'
+import GiscusComponent from '@giscus/react'
+import type { Mapping, BooleanString, InputPosition } from '@giscus/react'
 import { SITE_METADATA } from '~/data/site-metadata'
 
-export function Comments({ slug, className }: { slug: string; className?: string }) {
-  if (!SITE_METADATA.comments?.provider) {
-    return null
-  }
+interface GiscusConfig {
+  themeURL: string
+  theme: string
+  darkTheme: string
+  mapping: Mapping
+  repo: `${string}/${string}`
+  repositoryId: string
+  category: string
+  categoryId: string
+  reactions: BooleanString
+  metadata: BooleanString
+  inputPosition: InputPosition
+  lang: string
+}
+
+interface CommentsProps {
+  configs?: Partial<GiscusConfig>
+  className?: string
+}
+
+export function Comments({ configs, className }: CommentsProps) {
+  let defaultConfigs = SITE_METADATA.comments.giscusConfig as GiscusConfig
+  let {
+    themeURL,
+    theme,
+    darkTheme,
+    repo,
+    repositoryId,
+    category,
+    categoryId,
+    reactions,
+    metadata,
+    inputPosition,
+    lang,
+    mapping,
+  } = { ...defaultConfigs, ...configs }
+
+  let { theme: siteTheme, resolvedTheme } = useTheme()
+  let commentsTheme =
+    themeURL === ''
+      ? siteTheme === 'dark' || resolvedTheme === 'dark'
+        ? darkTheme
+        : theme
+      : themeURL
+
   return (
     <div id="comment" className={className}>
-      <CommentsComponent commentsConfig={SITE_METADATA.comments as CommentsConfig} slug={slug} />
+      <GiscusComponent
+        id="comments-container"
+        repo={repo}
+        repoId={repositoryId}
+        category={category}
+        categoryId={categoryId}
+        mapping={mapping}
+        reactionsEnabled={reactions}
+        emitMetadata={metadata}
+        inputPosition={inputPosition}
+        theme={commentsTheme}
+        lang={lang}
+        loading="lazy"
+      />
     </div>
   )
 }
