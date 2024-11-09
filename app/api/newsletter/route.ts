04e549f2fd877a0b4b31a825ea5bf558ee40e254
hta218 <hta218.dev@gmail.com>
2024-11-09 23:04:05 +0700

    refactor(newsletter): consolidate subscription logic into a single POST handler

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,21 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse, type NextRequest } from 'next/server'
-
-async function NewsletterAPIHandler(req: NextApiRequest, res: NextApiResponse) {
-  let { email } = req.body
-  if (!email) {
-    return res.status(400).json({ error: 'Email is required' })
-  }
-  try {
-    let response = await convertkitSubscribe(email)
-    if (response.status >= 400) {
-      res.status(response.status).json({ error: `There was an error subscribing to the list.` })
-    }
-    res.status(201).json({ message: 'Successfully subscribed to the newsletter' })
-  } catch (error) {
-    res.status(500).json({ error: error.message || error.toString() })
-  }
-}
 
 async function convertkitSubscribe(email: string) {
   let { CONVERTKIT_FORM_ID, CONVERTKIT_API_KEY } = process.env
@@ -26,7 +9,7 @@ async function convertkitSubscribe(email: string) {
   })
 }
 
-async function NewsletterRouteHandler(req: NextRequest) {
+export async function POST(req: NextRequest) {
   let { email } = await req.json()
   if (!email) {
     return NextResponse.json({ error: 'Email is required' }, { status: 400 })
@@ -46,18 +29,4 @@ async function NewsletterRouteHandler(req: NextRequest) {
   } catch (error) {
     return NextResponse.json({ error: error.message || error.toString() }, { status: 500 })
   }
-}
-
-// async function handler(req: NextApiRequest | NextRequest, res: NextApiResponse) {
-//   // For route handlers, 2nd argument contains the 'params' property instead of a response object
-//   if ('params' in res) {
-//     return await NewsletterRouteHandler(req as NextRequest)
-//   }
-//   return await NewsletterAPIHandler(req as NextApiRequest, res)
-// }
-
-// export { handler as POST }
-
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  return await NewsletterAPIHandler(req, res)
 }
