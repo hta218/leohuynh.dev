492768d07cca2b2b478621af31914762634c96b2
hta218 <hta218.dev@gmail.com>
2024-11-11 23:34:33 +0700

    feat(mdx): simplify pre component

--- components/mdx/pre.tsx
+++ components/mdx/pre.tsx
@@ -1,34 +1,18 @@
 'use client'
 
-import { Check, Copy } from 'lucide-react'
-import { useState, useRef, type ReactNode } from 'react'
-
-let timeoutId: ReturnType<typeof setTimeout> | undefined
+import { type ReactNode } from 'react'
+import { CopyCodeButton } from './copy-code-button'
 
 export function Pre({ children }: { children: ReactNode }) {
-  let textInput = useRef<HTMLDivElement>(null)
-  let [copied, setCopied] = useState(false)
-
   return (
-    <div
-      ref={textInput}
-      className="relative overflow-hidden rounded-lg border border-gray-100 dark:border-gray-700"
-    >
-      <button
-        aria-label="Copy code"
-        className="absolute right-3 top-3 hidden bg-solarized-light p-2 lg:inline-block"
-        onClick={() => {
-          if (textInput.current) {
-            navigator.clipboard.writeText(textInput.current.textContent!)
-            setCopied(true)
-            clearTimeout(timeoutId)
-            timeoutId = setTimeout(() => setCopied(false), 2000)
-          }
-        }}
-      >
-        {copied ? <Check className="h-4.5 w-4.5" /> : <Copy className="h-4.5 w-4.5" />}
-      </button>
-      <pre className="bg-solarized-light text-[#657B83] dark:bg-[#22272e]">{children}</pre>
+    <div className="relative overflow-hidden rounded-lg border border-gray-100 dark:border-gray-700">
+      <CopyCodeButton
+        parent="code-block"
+        className="absolute right-3 top-3 hidden lg:inline-block"
+      />
+      <pre className="bg-solarized-light text-code-block dark:bg-github-dark-dimmed">
+        {children}
+      </pre>
     </div>
   )
 }
