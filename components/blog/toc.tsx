2069cdd4790397dac1e4e73d3373dac20dc048de
hta218 <hta218.dev@gmail.com>
2024-11-13 17:41:18 +0700

    feat(blog): replace Table of Contents div with details element for better accessibility and UX

--- components/blog/toc.tsx
+++ components/blog/toc.tsx
@@ -1,4 +1,5 @@
 import { clsx } from 'clsx'
+import { ChevronRight } from 'lucide-react'
 import { Link } from '~/components/ui/link'
 
 type TocItem = {
@@ -9,8 +10,15 @@ type TocItem = {
 
 export function TableOfContents({ toc, className }: { toc: TocItem[]; className?: string }) {
   return (
-    <div className={clsx('space-y-4', className)}>
-      <h3 className="text-2xl font-semibold">On this page</h3>
+    <details className={clsx('space-y-4 [&_.chevron-right]:open:rotate-90', className)} open>
+      <summary className="flex cursor-pointer items-center gap-1 marker:content-none">
+        <ChevronRight
+          strokeWidth={1.5}
+          size={20}
+          className="chevron-right rotate-0 transition-transform"
+        />
+        <span className="text-lg font-medium">On this page</span>
+      </summary>
       <ul className="flex flex-col space-y-2">
         {toc.map(({ value, depth, url }) => (
           <li
@@ -22,6 +30,6 @@ export function TableOfContents({ toc, className }: { toc: TocItem[]; className?
           </li>
         ))}
       </ul>
-    </div>
+    </details>
   )
 }
