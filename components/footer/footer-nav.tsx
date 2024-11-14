d7762ef24c2fcc38f6bed4eec49915df3af21cb5
hta218 <hta218.dev@gmail.com>
2024-11-14 12:05:14 +0700

    fix(footer-nav): remove prefetch from footer link for better performance handling

--- components/footer/footer-nav.tsx
+++ components/footer/footer-nav.tsx
@@ -49,7 +49,7 @@ function FooterLink({ link }: { link: (typeof FOOTER_NAV_LINKS)[0] }) {
   let { href, title } = link
   let isExternal = href.startsWith('http')
   return (
-    <Link href={href} prefetch={!href.includes('/static/')}>
+    <Link href={href}>
       <GrowingUnderline
         data-umami-event={`footer-nav-${href.replace('/', '')}`}
         className="inline-flex items-center"
