c44d582bc4869856b78fbb6e5713285667135258
hta218 <hta218.dev@gmail.com>
2024-11-14 11:28:26 +0700

    feat(gradient-border): add customizable offset prop for improved positioning adjustments

--- components/ui/gradient-border.tsx
+++ components/ui/gradient-border.tsx
@@ -2,9 +2,11 @@ import { clsx } from 'clsx'
 
 export function GradientBorder({
   children,
+  offset = 16,
   className,
 }: {
   children: React.ReactNode
+  offset?: number
   className?: string
 }) {
   return (
@@ -14,17 +16,18 @@ export function GradientBorder({
         'border border-gray-200 dark:border-zinc-800',
         className,
       ])}
+      style={{ '--offset': offset + 'px' } as React.CSSProperties}
     >
       <span
         className={clsx([
-          'absolute -top-px right-4 h-px w-[40%]',
+          'absolute -top-px right-[--offset] h-px w-[40%]',
           'bg-gradient-to-r from-blue-500/0 via-indigo-500/40 to-indigo-500/0',
           'dark:from-indigo-400/0 dark:via-indigo-400/40 dark:to-indigo-400/0',
         ])}
       />
       <span
         className={clsx([
-          'absolute -left-px top-4 h-[40%] w-px',
+          'absolute -left-px top-[--offset] h-[40%] w-px',
           'bg-gradient-to-b from-indigo-500/0 via-indigo-500/40 to-indigo-500/0',
           'dark:from-indigo-400/0 dark:via-indigo-400/40 dark:to-indigo-400/0',
         ])}
