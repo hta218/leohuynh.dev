25d6819900ca89cea747c7ef6180fb1fd3ef0b24
hta218 <hta218.dev@gmail.com>
2025-06-14 10:06:42 +0700

    feat: add typography styles and parse function snippet for JavaScript/TypeScript

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -28,11 +28,11 @@ let isProduction = process.env.NODE_ENV === 'production'
 // heroicon mini link
 let icon = fromHtmlIsomorphic(
   `
-    <span class="content-header-link">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 linkicon">
-    <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
-    <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
-    </svg>
+    <span class="heading-anchor -ml-6 pr-1 opacity-0 hover:opacity-100 transition-opacity">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 inline-block">
+        <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
+        <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
+      </svg>
     </span>
   `,
   { fragment: true }
@@ -203,7 +203,7 @@ export default makeSource({
         {
           behavior: 'prepend',
           headingProperties: {
-            className: ['content-header'],
+            className: ['hover:[&_.heading-anchor]:opacity-100'],
           },
           content: icon,
         },
