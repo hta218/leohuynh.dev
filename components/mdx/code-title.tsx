188fc58c0a42600cedcf6bca017d514702b14174
hta218 <hta218.dev@gmail.com>
2025-06-19 11:39:41 +0700

    style: adjust CSS for code title component background color

--- components/mdx/code-title.tsx
+++ components/mdx/code-title.tsx
@@ -32,7 +32,7 @@ export function CodeTitle({ lang, title }: { lang: string; title: string }) {
       className={clsx([
         'remark-code-title',
         'flex items-center gap-2.5 truncate px-4 py-1 lg:py-2',
-        'bg-gray-200 text-gray-600 dark:bg-slate-700 dark:text-gray-300',
+        'bg-neutral-200 text-gray-600 dark:bg-slate-700 dark:text-gray-300',
         'rounded-t-lg border border-gray-100 dark:border-gray-700',
       ])}
     >
