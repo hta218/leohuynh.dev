befbf6630155d4e938c975574bf403fb725a764e
hta218 <hta218.dev@gmail.com>
2025-06-19 15:24:31 +0700

    style: update heading anchor icon for improved accessibility and aesthetics

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -25,14 +25,11 @@ import { extractTocHeadings } from './utils/remark-toc-headings'
 let root = process.cwd()
 let isProduction = process.env.NODE_ENV === 'production'
 
-// heroicon mini link
+// https://lucide.dev/icons/hash
 let icon = fromHtmlIsomorphic(
   `
-    <span class="heading-anchor -ml-6 pr-1 opacity-0 hover:opacity-100 transition-opacity">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 inline-block">
-        <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
-        <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
-      </svg>
+    <span class="heading-anchor -ml-6 inline-flex items-center pr-1 opacity-30 hover:opacity-100 transition-opacity text-gray-800 dark:text-gray-50">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-5 h-5 inline-block"><line x1="4" x2="20" y1="9" y2="9"/><line x1="4" x2="20" y1="15" y2="15"/><line x1="10" x2="8" y1="3" y2="21"/><line x1="16" x2="14" y1="3" y2="21"/></svg>
     </span>
   `,
   { fragment: true }
