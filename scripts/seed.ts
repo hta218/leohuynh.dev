5bf48dac3681a1d0d36b4e1bdf43429260275158
hta218 <hta218.dev@gmail.com>
2025-06-28 14:38:08 +0700

    feat: add RSS parsing functionality for Goodreads feed and update seedBooks function

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,6 +1,8 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import csv from 'csv-parser'
+import Parser from 'rss-parser'
+import { SITE_METADATA } from '~/data/site-metadata'
 import { upsertBooks, upsertManyMovies } from '~/db/queries'
 import {
   type InsertBook,
@@ -15,6 +17,87 @@ import type {
   OmdbMovie,
 } from '~/types/data'
 
+let parser = new Parser<{ [key: string]: unknown }, GoodreadsBook>({
+  customFields: {
+    item: [
+      'guid',
+      'title',
+      'link',
+      'pubDate',
+      ['book_id', 'id'],
+      ['book_image_url', 'bookImageUrl'],
+      ['book_small_image_url', 'bookSmallImageUrl'],
+      ['book_medium_image_url', 'bookMediumImageUrl'],
+      ['book_large_image_url', 'bookLargeImageUrl'],
+      ['book_description', 'bookDescription'],
+      ['author_name', 'authorName'],
+      ['isbn', 'isbn'],
+      ['user_name', 'userName'],
+      ['user_rating', 'userRating'],
+      ['user_read_at', 'userReadAt'],
+      ['user_date_added', 'userDateAdded'],
+      ['user_date_created', 'userDateCreated'],
+      ['user_shelves', 'userShelves'],
+      ['user_review', 'userReview'],
+      ['average_rating', 'averageRating'],
+      ['book_published', 'bookPublished'],
+    ],
+  },
+})
+
+export async function seedBooksUsingRss() {
+  if (SITE_METADATA.goodreadsFeedUrl) {
+    try {
+      console.log('Parsing Goodreads RSS feed...')
+      let data = await parser.parseURL(SITE_METADATA.goodreadsFeedUrl)
+
+      // Process book descriptions
+      for (let book of data.items) {
+        book.bookDescription = book.bookDescription
+          .replace(/<[^>]*(>|$)/g, '')
+          .replace(/\s\s+/g, ' ')
+          .replace(/^["|"]|["|"]$/g, '')
+          .replace(/\.([a-zA-Z0-9])/g, '. $1')
+        book.content = book.content.replace(/\n/g, '').replace(/\s\s+/g, ' ')
+      }
+
+      // Validate books data using Zod schema
+      let validBooks: InsertBook[] = []
+      for (let book of data.items) {
+        try {
+          let validatedBook = insertBookSchema.parse({
+            ...book,
+            updatedAt: new Date(),
+          })
+          validBooks.push(validatedBook)
+        } catch (error: unknown) {
+          console.log(
+            `❌ Invalid book data for "${book.title}":`,
+            error?.[0]?.message,
+          )
+        }
+      }
+
+      if (validBooks.length > 0) {
+        try {
+          let savedBooks = await upsertBooks(validBooks)
+          console.log(
+            `📚 ${savedBooks.length}/${data.items.length} books saved to database.`,
+          )
+        } catch (error) {
+          console.error(`❌ Error saving books to database: ${error.message}`)
+        }
+      } else {
+        console.log('📚 No valid books to save.')
+      }
+    } catch (error) {
+      console.error(`Error fetching the Goodreads RSS feed: ${error.message}`)
+    }
+  } else {
+    console.log('📚 No Goodreads RSS feed found.')
+  }
+}
+
 // Export Goodreads CSV data: https://www.goodreads.com/review/import
 const GOODREADS_CSV_FILE_PATH = path.join(
   process.cwd(),
@@ -22,7 +105,7 @@ const GOODREADS_CSV_FILE_PATH = path.join(
   'goodreads_library_export.csv',
 )
 
-export async function seedBooks() {
+export async function seedBooksByParsingCSV() {
   console.log('Processing Goodreads books from CSV...')
   if (!fs.existsSync(GOODREADS_CSV_FILE_PATH)) {
     console.log('📚 Goodreads CSV file not found.')
@@ -289,8 +372,9 @@ async function seedMovies() {
 }
 
 export async function seed() {
-  await seedMovies()
-  await seedBooks()
+  // await seedMovies()
+  // await seedBooksByParsingCSV()
+  await seedBooksUsingRss()
 }
 
 seed()
