1231e2fff49c5887162facce8db3e04fb254c28c
hta218 <hta218.dev@gmail.com>
2025-06-28 16:34:51 +0700

    style(typography): adjust heading anchor icon sizes for better visibility

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -28,8 +28,24 @@ let isProduction = process.env.NODE_ENV === 'production'
 // https://lucide.dev/icons/hash
 let icon = fromHtmlIsomorphic(
   `
-    <span class="heading-anchor -ml-6 inline-flex items-center pr-1 opacity-30 hover:opacity-100 transition-opacity text-gray-800 dark:text-gray-50">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-5 h-5 inline-block"><line x1="4" x2="20" y1="9" y2="9"/><line x1="4" x2="20" y1="15" y2="15"/><line x1="10" x2="8" y1="3" y2="21"/><line x1="16" x2="14" y1="3" y2="21"/></svg>
+    <span class="heading-anchor inline-flex items-center opacity-30 hover:opacity-100 transition-opacity text-gray-800 dark:text-gray-50">
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="24" 
+        height="24" 
+        viewBox="0 0 24 24" 
+        fill="currentColor" 
+        stroke="currentColor" 
+        stroke-width="2" 
+        stroke-linecap="round" 
+        stroke-linejoin="round" 
+        className="inline-block"
+      >
+        <line x1="4" x2="20" y1="9" y2="9"/>
+        <line x1="4" x2="20" y1="15" y2="15"/>
+        <line x1="10" x2="8" y1="3" y2="21"/>
+        <line x1="16" x2="14" y1="3" y2="21"/>
+      </svg>
     </span>
   `,
   { fragment: true },
