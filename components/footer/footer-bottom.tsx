86674a025014fd25fbb1fb8020cf28b66eb8e7f2
hta218 <hta218.dev@gmail.com>
2025-06-28 23:28:26 +0700

    feat(footer): update FooterBottom to conditionally render BuiltWith or SpotifyNowPlaying

--- components/footer/footer-bottom.tsx
+++ components/footer/footer-bottom.tsx
@@ -1,11 +1,17 @@
+'use client'
+
 import { clsx } from 'clsx'
-import { AreaChart, Rss } from 'lucide-react'
+import { usePathname } from 'next/navigation'
 import { Link } from '~/components/ui/link'
 import { SpotifyNowPlaying } from '~/components/ui/now-playing'
 import { SITE_METADATA } from '~/data/site-metadata'
 import MadeInVietNam from '~/icons/miv.svg'
+import { BuiltWith } from './built-with'
 
 export function FooterBottom() {
+  let pathname = usePathname()
+  let isHomePage = pathname === '/'
+
   return (
     <div
       className={clsx([
@@ -14,30 +20,20 @@ export function FooterBottom() {
         'border-t border-gray-200 dark:border-gray-700',
       ])}
     >
-      <SpotifyNowPlaying
-        className="w-full justify-center truncate [--artist-color:var(--color-gray-500)] md:max-w-[50%] md:justify-start"
-        songEffect="underline"
-        showCover
-      />
-      <div className="flex items-center">
-        <Link href={SITE_METADATA.siteRepo}>
-          <span data-umami-event="made-in-vietnam">
-            <MadeInVietNam />
-          </span>
-        </Link>
-        {/* <span className="mx-2.5 text-gray-400">|</span>
-        <Link href="/feed.xml" aria-label="RSS Feed" data-umami-event="rss-feed" prefetch={false}>
-          <Rss strokeWidth={1.5} size={20} />
-        </Link>
-        <Link
-          href={SITE_METADATA.analytics.umamiAnalytics.shareUrl}
-          aria-label="Open analytics"
-          data-umami-event="footer-analytics"
-          className="ml-2"
-        >
-          <AreaChart strokeWidth={1.5} size={22} />
-        </Link> */}
-      </div>
+      {isHomePage ? (
+        <BuiltWith />
+      ) : (
+        <SpotifyNowPlaying
+          className="w-full justify-center truncate [--artist-color:var(--color-gray-500)] md:max-w-[50%] md:justify-start"
+          songEffect="underline"
+          showCover
+        />
+      )}
+      <Link href={SITE_METADATA.siteRepo}>
+        <span data-umami-event="made-in-vietnam">
+          <MadeInVietNam />
+        </span>
+      </Link>
     </div>
   )
 }
