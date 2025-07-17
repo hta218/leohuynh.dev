5701e4f5513f74940228ad9f6952d35cd34a24a5
hta218 <hta218.dev@gmail.com>
2025-07-18 06:43:22 +0700

    feat(terminal): update layout of HomePage for improved structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import { Terminal } from '~/components/home-page/terminal'
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen relative flex items-center justify-center p-4">
+    <div className="min-h-screen relative flex flex-col items-center p-4">
+      <div className="h-40" />
       <Terminal />
     </div>
   )
