abdc31049e8e8bf74cd928de840d2ebc972eaf7c
hta218 <hta218.dev@gmail.com>
2025-07-17 13:57:31 +0700

    feat(terminal): refactor Terminal component and integrate ASCII art rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import { TerminalHome } from '~/components/home-page/terminal'
+import { Terminal } from '~/components/home-page/terminal'
 
 export default function HomePage() {
-  return <TerminalHome />
+  return (
+    <div className="min-h-screen relative flex items-center justify-center p-4">
+      <Terminal />
+    </div>
+  )
 }
