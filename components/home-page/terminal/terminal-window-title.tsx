b40196bf9813e16c565b8137c2ad6a2bd0ca6b22
hta218 <hta218.dev@gmail.com>
2025-08-09 17:00:46 +0700

    feat(terminal): add last commit display and status indicators in terminal title

--- components/home-page/terminal/terminal-window-title.tsx
+++ components/home-page/terminal/terminal-window-title.tsx
@@ -1,11 +1,21 @@
 'use client'
 
-import { CheckCheck } from 'lucide-react'
+import { CheckCheck, Circle, X } from 'lucide-react'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import useSWR from 'swr'
 import { Twemoji } from '~/components/ui/twemoji'
+import { SITE_METADATA } from '~/data/site-metadata'
+import type { CommitState, GithubRepository } from '~/types/data'
+import { fetcher } from '~/utils/misc'
 
 export function TerminalWindowTitle() {
   const pathname = usePathname()
+  let siteRepo = SITE_METADATA.siteRepo.replace('https://github.com/', '')
+  let { data: repo } = useSWR<GithubRepository>(
+    `/api/github?repo=${siteRepo}`,
+    fetcher,
+  )
 
   const getDisplayPath = () => {
     if (pathname === '/') return '/leohuynh.dev'
@@ -18,8 +28,44 @@ export function TerminalWindowTitle() {
       <Twemoji emoji="flag-vietnam" />
       <span className="-ml-0.5">{getDisplayPath()}</span>
       <span className="mx-2 opacity-50">|</span>
-      <span>[main]</span>
-      <CheckCheck size={16} className="text-green-500 ml-2" />
+      {repo?.lastCommit && (
+        <>
+          <Link
+            href={repo.lastCommit.url}
+            target="_blank"
+            className="mr-2 hover:underline underline-offset-4"
+            data-terminal-accent
+            title={repo.lastCommit.message}
+          >
+            <span data-umami-event="repo-last-commit">
+              #{repo.lastCommit.abbreviatedOid}
+            </span>
+          </Link>
+          <CommitStatus status={repo.lastCommit.status.state} />
+        </>
+      )}
     </>
   )
+}
+
+function CommitStatus({ status }: { status: CommitState }) {
+  switch (status) {
+    case 'EXPECTED':
+    case 'SUCCESS':
+      return <CheckCheck size={16} strokeWidth={2} className="text-green-700" />
+    case 'PENDING':
+      return (
+        <Circle
+          size={12}
+          strokeWidth={1.5}
+          fill="green"
+          className="animate-pulse text-[green]"
+        />
+      )
+    case 'ERROR':
+    case 'FAILURE':
+      return <X size={16} strokeWidth={2} className="text-red-700" />
+    default:
+      return null
+  }
 }
