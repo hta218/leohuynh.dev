81b94f91e7e4bfe64dba463a9d4ccbc9cdaea987
hta218 <hta218.dev@gmail.com>
2025-08-28 23:50:14 +0700

    refactor(activities): update loading skeleton component

--- components/home-page/activities/skeleton.tsx
+++ components/home-page/activities/skeleton.tsx
@@ -1,13 +1,13 @@
 export function ActivitiesSkeleton() {
   return (
-    <div className="space-y-4 md:space-y-8 pt-8 animate-pulse">
+    <div className="space-y-4 md:space-y-8 pt-8">
       <div className="space-y-2">
         <h3 className="text-2xl font-bold sm:text-2xl sm:leading-10 md:text-4xl">
           Side quests and activities
         </h3>
       </div>
       <div className="border-t border-gray-200 dark:border-gray-700" />
-      <div className="pt-2 md:pt-0 space-y-6">
+      <div className="pt-2 md:pt-0 space-y-6 animate-pulse">
         <div className="space-y-2">
           <div className="h-5 bg-gray-200 dark:bg-gray-700 rounded w-32" />
           <div className="flex items-center gap-3">
